4ec585040a88b3a383f38f0019fd019b96b9fcc0
Joerg Schneider <schneider.joerg@bcg.com>
2019-04-26 10:48:14 +0200

    Extend config loader to load specific section and add test for it

--- tests/core/test_core.py
+++ tests/core/test_core.py
@@ -1,4 +1,5 @@
 import yieldengine.core
+import pytest
 
 
 def test_get_global_config():
@@ -6,3 +7,11 @@ def test_get_global_config():
     config = yieldengine.core.get_global_config()
     # yaml.safe_load() returns a list of items/dicts
     assert type(config) == list, "Expected a list"
+
+    # test loading of a specific section
+    inputfile_config = yieldengine.core.get_global_config(section="inputfile")
+    assert type(inputfile_config) in (list, dict), "Expected a list/or dict for section inputfile"
+
+    # test raising of ValueError exception for invalid section name
+    with pytest.raises(expected_exception=ValueError):
+        invalid_config = yieldengine.core.get_global_config(section="does_not_exist!")
--- yieldengine/core/__init__.py
+++ yieldengine/core/__init__.py
@@ -1,12 +1,22 @@
 import os
-
 import yaml
+import yieldengine
 
 
-def get_global_config():
-    base_dir = os.path.dirname(__file__)
+def get_global_config(section: str = None):
+    base_dir = os.path.dirname(yieldengine.__file__)
     config_file_path = os.path.join(base_dir, "yield_engine_config.yml")
     # todo: handle exception
     # todo: validate yaml structure (known/required keys)
     with open(config_file_path, 'r') as f:
-        return yaml.safe_load(f)
+        config = yaml.safe_load(f)
+        if section is None:
+            return config
+        else:
+            for element in config:
+                if type(element) == dict:
+                    for key in element.keys():
+                        if key == section:
+                            return element[key]
+
+            raise ValueError("Section %s not found in global config!" % section)
