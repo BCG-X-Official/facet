4d6260744ca48e10d87aa25a2d6a1b60d5046a86
Joerg Schneider <schneider.joerg@bcg.com>
2019-04-29 14:38:54 +0200

    Rewrite datasplitter as class
    Add scikitlearn compatibility through get_train_test_splits_as_indices()
    Adapt tests

--- tests/preprocessing/test_datasplitter.py
+++ tests/preprocessing/test_datasplitter.py
@@ -1,5 +1,6 @@
 import pytest
 import pandas as pd
+import numpy as np
 import os
 import yieldengine.core
 
@@ -23,13 +24,42 @@ def test_dataframe():
     )
 
 
-def test_get_train_test_splits(test_dataframe: pd.DataFrame):
-    from yieldengine.preprocessing import datasplitter
+def test_datasplitter_init(test_dataframe: pd.DataFrame):
+    from yieldengine.preprocessing.datasplitter import Datasplitter
+
+    # check erroneous inputs
+    #   - test_ratio = 0
+    with pytest.raises(expected_exception=ValueError):
+        Datasplitter(num_samples=100, test_ratio=0.0)
+
+    #   - test_ratio < 0
+    with pytest.raises(expected_exception=ValueError):
+        Datasplitter(num_samples=100, test_ratio=-0.1)
+
+    #   - test_ratio > 1
+    with pytest.raises(expected_exception=ValueError):
+        Datasplitter(num_samples=100, test_ratio=1.00001)
+
+    #   - 0 samples per fold
+    with pytest.raises(expected_exception=ValueError):
+        Datasplitter(num_samples=len(test_dataframe), test_ratio=0.00001)
+
+    #   - (0 samples)
+    with pytest.raises(expected_exception=ValueError):
+        Datasplitter(num_samples=0)
+
+    #   - (#samples not specified)
+    with pytest.raises(expected_exception=ValueError):
+        Datasplitter()
+
+
+def test_get_train_test_splits_as_dataframe(test_dataframe: pd.DataFrame):
+    from yieldengine.preprocessing.datasplitter import Datasplitter
+
+    my_ds = Datasplitter(num_samples=len(test_dataframe), test_ratio=0.2, num_folds=50)
 
     list_of_train_test_splits = list(
-        datasplitter.get_train_test_splits(
-            input_dataset=test_dataframe, test_ratio=0.2, num_folds=50
-        )
+        my_ds.get_train_test_splits_as_dataframes(input_dataset=test_dataframe)
     )
 
     # assert we get 50 folds
@@ -63,38 +93,39 @@ def test_get_train_test_splits(test_dataframe: pd.DataFrame):
         combined = pd.concat([train_set, test_set], axis=0).sort_index()
         assert test_dataframe.equals(combined)
 
-    # check erroneous inputs
-    #   - test_ratio = 0
-    with pytest.raises(expected_exception=ValueError):
-        list(
-            datasplitter.get_train_test_splits(
-                input_dataset=test_dataframe, test_ratio=0.0
-            )
-        )
-    #   - test_ratio < 0
-    with pytest.raises(expected_exception=ValueError):
-        list(
-            datasplitter.get_train_test_splits(
-                input_dataset=test_dataframe, test_ratio=-0.1
-            )
-        )
 
-    #   - test_ratio > 1
-    with pytest.raises(expected_exception=ValueError):
-        list(
-            datasplitter.get_train_test_splits(
-                input_dataset=test_dataframe, test_ratio=1.00001
-            )
-        )
+def test_get_train_test_splits_as_indices(test_dataframe: pd.DataFrame):
+    from yieldengine.preprocessing.datasplitter import Datasplitter
 
-    #   - 0 samples per fold
-    with pytest.raises(expected_exception=ValueError):
-        list(
-            datasplitter.get_train_test_splits(
-                input_dataset=test_dataframe, test_ratio=0.00001
-            )
-        )
+    test_folds = 200
 
-    #   - empty input dataframe (0 samples)
-    with pytest.raises(expected_exception=ValueError):
-        list(datasplitter.get_train_test_splits(input_dataset=pd.DataFrame()))
+    my_ds = Datasplitter(num_samples=123456, test_ratio=0.2, num_folds=test_folds)
+
+    list_of_train_test_splits = list(my_ds.get_train_test_splits_as_indices())
+
+    list_of_train_test_splits_2 = list(my_ds.get_train_test_splits_as_indices())
+
+    assert len(list_of_train_test_splits) == len(
+        list_of_train_test_splits_2
+    ), "The number of folds should be stable!"
+
+    for f1, f2 in zip(list_of_train_test_splits, list_of_train_test_splits_2):
+        assert np.array_equal(f1[0], f2[0]), "Fold indices should be stable!"
+        assert np.array_equal(f1[1], f2[1]), "Fold indices should be stable!"
+
+    # now test the opposite: resample() should change fold indices on the next call of get_train_test_splits...
+    my_ds.resample()
+
+    list_of_train_test_splits_3 = list(my_ds.get_train_test_splits_as_indices())
+
+    # due to randomness, we need to check this with a threshold
+    # we allow 2 folds to be randomly the same
+    randomly_same_allowed_threshold = 2
+    num_different_folds = 0
+    for f1, f2 in zip(list_of_train_test_splits, list_of_train_test_splits_3):
+        if not np.array_equal(f1[0], f2[0]) and not np.array_equal(f1[1], f2[1]):
+            num_different_folds = num_different_folds + 1
+
+    assert num_different_folds >= (
+        test_folds - randomly_same_allowed_threshold
+    ), "There are too many equal folds!"
