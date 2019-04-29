2dc70ebbdcab900050ec02b6d00294c4c2718a52
Joerg Schneider <schneider.joerg@bcg.com>
2019-04-29 14:40:08 +0200

    Some sphinx changes + rebuild docs
     - conf.py: rearrange code, add autoclass config
     - changed docs: of preprocessing module after last changes to datasplitter

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -13,13 +13,14 @@
 # ensure, yieldengine modules are found:
 import os
 import sys
-sys.path.insert(0, os.path.abspath('./../../'))
+
+sys.path.insert(0, os.path.abspath("./../../"))
 
 # -- Project information -----------------------------------------------------
 
-project = 'yield-engine'
-copyright = '2019, Joerg Schneider'
-author = 'Joerg Schneider'
+project = "yield-engine"
+copyright = "2019, Joerg Schneider"
+author = "Joerg Schneider"
 
 
 # -- General configuration ---------------------------------------------------
@@ -27,11 +28,10 @@ author = 'Joerg Schneider'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc'
-]
+extensions = ["sphinx.ext.autodoc"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -44,9 +44,11 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
+
+autoclass_content = "class"
