7da9ffda99f92f9557acd012536dc37d954344c9
Joerg Schneider <schneider.joerg@bcg.com>
2019-04-29 09:27:14 +0200

    Add missing sphinx autodoc extension to conf.py

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -27,7 +27,7 @@ author = 'Joerg Schneider'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+extensions = ['sphinx.ext.autodoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
