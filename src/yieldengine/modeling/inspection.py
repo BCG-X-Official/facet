edba0f76d1275801267724340b5906c33cfec5c4
Joerg Schneider <schneider.joerg@bcg.com>
2019-05-07 16:41:38 +0200

    add ModelPredictor and adapt ModelInspector

--- src/yieldengine/modeling/inspection.py
+++ src/yieldengine/modeling/inspection.py
@@ -1,28 +1,68 @@
+from typing import *
+
 import pandas as pd
 from sklearn.base import BaseEstimator
 from sklearn.pipeline import Pipeline
+
 from yieldengine.loading.sample import Sample
 from yieldengine.modeling.validation import CircularCrossValidator
 
 
-class ModelInspector:
+class ModelPredictor:
     def __init__(
         self,
-        model: BaseEstimator,
+        estimator: BaseEstimator,
         cv: CircularCrossValidator,
         preprocessing: Pipeline = None,
     ) -> None:
+        self._estimator = estimator
+        self._cv = cv
+        self._preprocessing = preprocessing
+
+    def fit(self, X: pd.DataFrame, y: pd.Series) -> BaseEstimator:
+        pass
+
+    def predict(self, X: pd.DataFrame) -> pd.Series:
+        pass
+
+    def foldwise_fit_predict(
+        self, observations: Sample
+    ) -> Generator[Tuple[Pipeline, pd.Series]]:
+        # 1. wrap self._estimator in a Pipeline if preprocessing is not None
+        # 2. get folds splits across observations using self.cv
+        # 3. for each split
+        #       - fit Pipeline on X of train set
+        #       - predict y for test set
+        #       - yield deepcopy(Pipeline), predictions (!ensure predictions has index!)
+        pass
+
+
+class ModelInspector:
+    def __init__(self, predictor: ModelPredictor) -> None:
+        self._predictor = predictor
         pass
 
     def shap_value_matrix(self, observations: Sample) -> pd.DataFrame:
-        # for model:
-        #   explainer = shap.TreeExplainer(model)
-        #   shap_values = explainer.shap_values(X)
+        for estimator, predictions in self._predictor.foldwise_fit_predict(
+            observations=observations
+        ):
+            pass
+            # 1.) init the Shap explainer:
+            # explainer = shap.TreeExplainer(estimator)
 
-        # arrange in matrix, correct/enhance feature names
-        # convert to pandas DataFrame
+            # 2.) get X to explain
+            # x_to_explain = observations.features.iloc[predictions.index]
 
-        pass
+            # 3.) run explainer
+            # shap_values = explainer.shap_values(x_to_explain)
+
+            # 4.) collect shap_values by index
+
+        # 5.) average/combine/aggregate Shap values when overlapping index
+
+        # 6.) convert to pandas DataFrame
+
+        return pd.DataFrame()
 
     def feature_dependencies(self, observations: Sample) -> pd.DataFrame:
         # use shap_value_matrix()
