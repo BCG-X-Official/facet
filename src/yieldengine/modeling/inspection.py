66cffc8af2d55d1e6a29130e6461b53c2366dfc2
Ittner Jan <ittner.jan@bcg.com>
2019-05-08 09:19:48 +0200

    update stub of class ModelInspector

--- src/yieldengine/modeling/inspection.py
+++ src/yieldengine/modeling/inspection.py
@@ -7,24 +7,24 @@ from yieldengine.loading.sample import Sample
 
 
 class ModelInspector:
-    __slots__ = ["_pipeline", "_cv", "_sample", "_shap_explainer"]
+    __slots__ = ["__pipeline", "__cv", "__sample", "__shap_explainer"]
 
     F_OBSERVATION_ID = "observation"
     F_FOLD_START = "fold_start"
     F_PREDICTION = "prediction"
-    F_MODEL = "model"
 
     def __init__(
         self, pipeline: Pipeline, cv: BaseCrossValidator, sample: Sample
     ) -> None:
         if not isinstance(ModelInspector._get_estimator(pipeline), BaseEstimator):
             raise ValueError("arg pipeline does not end with estimator")
-        self._pipeline = pipeline
-        self._cv = cv
-        self._sample = sample
+        self.__pipeline = pipeline
+        self.__cv = cv
+        self.__sample = sample
 
         # init the Shap explainer:
-        # self._shap_explainer = shap.TreeExplainer(estimator) or other (is Shap able to
+        # self.__shap_explainer = shap.TreeExplainer(estimator) or other (is Shap
+        # able to
         # determine the best explainer based on model type?)
 
     @staticmethod
@@ -33,18 +33,18 @@ class ModelInspector:
 
     @property
     def pipeline(self) -> Pipeline:
-        return self._pipeline
+        return self.__pipeline
 
     @property
     def cv(self) -> BaseCrossValidator:
-        return self._cv
+        return self.__cv
 
     @property
     def sample(self) -> Sample:
-        return self._sample
+        return self.__sample
 
     @property
-    def _estimator(self) -> BaseEstimator:
+    def estimator(self) -> BaseEstimator:
         return self._get_estimator(self.pipeline)
 
     def predictions_for_all_samples(self) -> pd.DataFrame:
@@ -53,8 +53,7 @@ class ModelInspector:
         in the test set. The result is a data frame with one row per prediction, and
         columns F_OBSERVATION_ID (the index of the observation in the sample),
         F_FOLD_START (the numerical index of the start of the test set in the current
-        fold),  F_PREDICTION (the predicted value for the given observation and fold),
-        and F_MODEL (the fitted estimator used to make the prediction).
+        fold),  F_PREDICTION (the predicted value for the given observation and fold)
 
         Note that there can be multiple prediction rows per observation if the test
         folds overlap.
@@ -68,6 +67,14 @@ class ModelInspector:
         #       - clone the estimator using function sklearn.base.clone()
         #       - fit the estimator on the X and y of train set
         #       - predict y for test set
+        pass
+
+    def get_estimator(self, fold: int) -> BaseEstimator:
+        """
+        :param fold: start index of test fold
+        :return: the estimator that was used to predict the dependent vartiable of
+        the test fold
+        """
         pass
 
     def shap_value_matrix(self) -> pd.DataFrame:
