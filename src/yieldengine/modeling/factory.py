7db345f408264def3f8c0309a13dd6178a82115f
Joerg Schneider <schneider.joerg@bcg.com>
2019-05-09 10:49:04 +0200

    add basic stubs for modeling.factory.ModelFactory

---
+++ src/yieldengine/modeling/factory.py
@@ -0,0 +1,34 @@
+from abc import ABC, abstractmethod
+
+from sklearn.base import BaseEstimator
+from sklearn.pipeline import Pipeline
+from typing import *
+
+
+class ModelFactory(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def make_pipeline(self, estimator: BaseEstimator) -> Pipeline:
+        raise NotImplementedError("call to abstract make_pipeline()")
+
+    @abstractmethod
+    def make_multi_estimator_pipeline(
+        self, estimators: Iterable[BaseEstimator]
+    ) -> Pipeline:
+        raise NotImplementedError("call to abstract make_pipeline()")
+
+
+class PreprocessedModelFactory(ModelFactory):
+    def __init__(self, preprocessing: Pipeline):
+        super().__init__()
+        self._preprocessing = preprocessing
+
+    def make_pipeline(self, estimator: BaseEstimator) -> Pipeline:
+        pass
+
+    def make_multi_estimator_pipeline(
+        self, estimators: Iterable[BaseEstimator]
+    ) -> Pipeline:
+        pass
