d7b81ebc5a9071dcf3e828a0feed40390356b0aa
Ittner Jan <ittner.jan@bcg.com>
2019-05-23 18:05:28 +0200

    refactor code for linkage trees and dendrogram drawing

--- src/yieldengine/feature/dendrogram.py
+++ src/yieldengine/feature/dendrogram.py
@@ -1,35 +1,90 @@
-from yieldengine.feature.linkage import LinkageNode, LinkageTree
+from typing import NamedTuple
+
+from yieldengine.feature.linkage import LeafNode, LinkageNode, LinkageTree, Node
+
+
+class _SubtreeInfo(NamedTuple):
+    height: int
+    weight: float
 
 
 class DendrogramDrawer:
     # note: draws for left->right oriented version (i.e. root node on the right)
     def __init__(self, linkage: LinkageTree):
-        self._linkage = linkage
+        self._linkage_tree = linkage
 
     def draw(self, width: int, height: int) -> None:
         # initialize figure/canvas using supplied bounds
+        pass
+
         # run _draw_dendrogram
-        pass
+        self._draw_dendrogram(node=self._linkage_tree.root(), y=0, width_relative=1.0)
 
-    def _draw_dendrogram(self, node: LinkageNode, y: int) -> int:
+    def _draw_dendrogram(
+        self, node: Node, y: int, width_relative: float
+    ) -> _SubtreeInfo:
         # returns height
-        if self._linkage.is_leaf(node):
-            self._draw_link_leg(node=node, y=y)
+        if isinstance(node, LeafNode):
+            height = self._draw_leaf_label(y=y, label=node.label)
+
+            self._draw_link_leg(
+                x1_relative=0.0,
+                x2_relative=width_relative,
+                y=y + height // 2,
+                weight=node.weight,
+            )
+
+            return _SubtreeInfo(height=height, weight=node.weight)
+
+        elif isinstance(node, LinkageNode):
+            child_left, child_right = self._linkage_tree.children(node=node)
+            info_left = self._draw_dendrogram(
+                node=child_left, y=y, width_relative=node.children_distance
+            )
+            info_right = self._draw_dendrogram(
+                node=child_right,
+                y=y + info_left.height,
+                width_relative=node.children_distance,
+            )
+
+            info = _SubtreeInfo(
+                height=info_left.height + info_right.height,
+                weight=info_left.weight + info_right.weight,
+            )
+
+            self._draw_link_connector(
+                x_relative=node.children_distance,
+                y1=y + info_left.height // 2,
+                y2=y + info_left.height + info_right.height // 2,
+                weight=info.weight,
+            )
+
+            self._draw_link_leg(
+                x1_relative=node.children_distance,
+                x2_relative=width_relative,
+                y=y + info.height // 2,
+                weight=info.weight,
+            )
+
+            return info
+
         else:
-            pass
+            raise TypeError(f"unknown node type: {type(node)}")
+
+    def _draw_leaf_label(self, y: int, label: str) -> int:
         pass
 
-    def _draw_link_leg(self, node: LinkageNode, y: int) -> int:
+    def _draw_link_leg(
+        self, x1_relative: float, x2_relative: float, y: int, weight: float
+    ) -> int:
         # get colour by DendrogramDrawer.color(node)
         # draw coloured line from x=[0,node.link_distance] on y=y
         # returns height
         pass
 
-    def _draw_link_leg_connector(self, y1: int, y2: int, x: int, color: str) -> None:
-        pass
-
-    def _draw_label(self, text: str, y: int) -> int:
-        # returns height
+    def _draw_link_connector(
+        self, x_relative: float, y1: int, y2: int, weight: float
+    ) -> None:
         pass
 
     @staticmethod
--- src/yieldengine/feature/linkage.py
+++ src/yieldengine/feature/linkage.py
@@ -1,71 +1,113 @@
+from abc import ABC, abstractmethod
 from typing import *
 
 import numpy as np
 
 
-class LinkageTree:
-    def __init__(self, linkage_matrix: np.ndarray, leaf_labels: Sequence[str]) -> None:
-        self._linkage_matrix = linkage_matrix
-        self._leaf_labels = leaf_labels
-
-    def root(self) -> "LinkageNode":
-        return LinkageNode(
-            index=len(self._linkage_matrix) * 2 - 1,
-            label="",
-            importance=1.0,
-            link_distance=self._linkage_matrix[-1][2],
-        )
-
-    def direct_children(self, node: "LinkageNode") -> Optional[Iterable["LinkageNode"]]:
-        if self.is_leaf(node=node):
-            return None
-        else:
-            pass
-
-    def all_children(self, node: "LinkageNode") -> Optional[Iterable["LinkageNode"]]:
-        if self.is_leaf(node=node):
-            return None
-        else:
-            pass
-
-    def is_leaf(self, node: "LinkageNode") -> bool:
-        return node.index < len(self._linkage_matrix)
-
-    def feature_importance(self, node: "LinkageNode") -> float:
-        # if is_leaf -> single scaled median of Shap values for the feature, in [0,1]
-        # if !is_leaf -> aggregated scaled median of Shap values for features, in [0,1]
-        pass
-
-    def label(self, node: "LinkageNode") -> Optional[str]:
-        if self.is_leaf(node=node):
-            pass
-        else:
-            # only leafs have labels
-            return None
-
-
-class LinkageNode:
-    def __init__(
-        self, index: int, link_distance: Optional[float], importance: float, label: str
-    ) -> None:
+class Node(ABC):
+    def __init__(self, index: int) -> None:
+        if type(self) == Node:
+            raise TypeError("cannot instantiate abstract class Node")
         self._index = index
-        self._link_distance = link_distance
-        self._importance = importance
-        self._label = label
-        pass
 
     @property
     def index(self) -> int:
         return self._index
 
     @property
-    def importance(self) -> float:
-        return self._importance
+    @abstractmethod
+    def is_leaf(self) -> bool:
+        pass
+
+
+class LinkageNode(Node):
+    __slots__ = ["_children_distance"]
+
+    def __init__(self, index: int, children_distance: Optional[float]) -> None:
+        super().__init__(index=index)
+        self._children_distance = children_distance
+
+    @property
+    def children_distance(self) -> float:
+        return self._children_distance
+
+    @property
+    def is_leaf(self) -> bool:
+        return False
+
+
+class LeafNode(Node):
+    __slots__ = ["_weight", "_label"]
+
+    def __init__(self, index: int, weight: float, label: str) -> None:
+        super().__init__(index=index)
+        self._weight = weight
+        self._label = label
+
+    @property
+    def weight(self) -> float:
+        return self._weight
 
     @property
     def label(self) -> str:
         return self._label
 
     @property
-    def link_distance(self) -> float:
-        return self._link_distance
+    def is_leaf(self) -> bool:
+        return True
+
+
+class LinkageTree:
+
+    F_CHILD_LEFT = 0
+    F_CHILD_RIGHT = 1
+    F_CHILDREN_DISTANCE = 2
+    F_N_DESCENDANTS = 3
+
+    def __init__(
+        self,
+        scipy_linkage_matrix: np.ndarray,
+        leaf_labels: Sequence[str],
+        leaf_weights: Sequence[float],
+    ) -> None:
+        # one row of the linkage matrix is a quadruple:
+        # (
+        #    <index of left child>,
+        #    <index of right child>,
+        #    <distance between children>,
+        #    <number of descendant nodes, from direct children down to leaf nodes>
+        # )
+        self._linkage_matrix = scipy_linkage_matrix
+        self._leaf_labels = leaf_labels
+        self._leaf_weights = leaf_weights
+
+    def root(self) -> LinkageNode:
+        return LinkageNode(
+            index=len(self._linkage_matrix) * 2 - 1,
+            children_distance=self._linkage_matrix[-1][LinkageTree.F_CHILDREN_DISTANCE],
+        )
+
+    def _linkage_for_node(self, index: int):
+        return self._linkage_matrix[index - len(self._linkage_matrix)]
+
+    def node(self, index: int) -> Node:
+        if index < len(self._linkage_matrix):
+            return LeafNode(
+                index=index,
+                weight=self._leaf_weights[index],
+                label=self._leaf_labels[index],
+            )
+        else:
+            return LinkageNode(
+                index=index,
+                children_distance=self._linkage_for_node(index)[
+                    LinkageTree.F_CHILDREN_DISTANCE
+                ],
+            )
+
+    def children(self, node: LinkageNode) -> Optional[Tuple[LinkageNode, LinkageNode]]:
+        if node.is_leaf:
+            return None
+        else:
+            node_linkage = self._linkage_for_node(node.index)
+            return node_linkage[LinkageTree.F_CHILD_LEFT, LinkageTree.F_CHILD_RIGHT]
