a00192801c347b1d7e90a1a3c05cfde9472d800c
Ittner Jan <ittner.jan@bcg.com>
2019-05-27 07:43:17 +0200

    add FeatMapStyle; use log x axis and log weights for color gradients

--- src/yieldengine/dendrogram/style.py
+++ src/yieldengine/dendrogram/style.py
@@ -1,8 +1,10 @@
 import logging
+import math
 from typing import *
 
 from matplotlib import cm
 from matplotlib.axes import Axes
+from matplotlib.ticker import ScalarFormatter
 
 from yieldengine.dendrogram import DendrogramStyle
 
@@ -12,41 +14,48 @@ RgbaColor = Tuple[float, float, float, float]
 
 
 class MatplotStyle(DendrogramStyle):
-    __slots__ = ["_ax"]
+    __slots__ = ["_ax", "_cm", "_min_weight"]
 
-    def __init__(self, ax: Axes):
+    def __init__(
+        self, ax: Axes, min_weight: float = 0.001, log_scale: bool = True
+    ) -> None:
         super().__init__()
         self._ax = ax
+        if log_scale:
+            ax.set_xscale("log")
+        else:
+            ax.set_xscale("linear")
+
+        scalar_formatter = ScalarFormatter()
+        ax.xaxis.set_major_formatter(scalar_formatter)
+        ax.xaxis.set_minor_formatter(scalar_formatter)
+
+        ax.ticklabel_format(axis="x", scilimits=(-3, 3))
+        self._cm = cm.get_cmap(name="plasma", lut=256)
+        self._min_weight = min_weight
 
     def draw_title(self, title: str) -> None:
         self._ax.set_title(label=title)
-
-    def _draw_line(
-        self, x1: float, x2: float, y1: float, y2: float, color: RgbaColor
-    ) -> None:
-        self._ax.plot((x1, x2), (y1, y2), color=color)
-
-
-class LineStyle(MatplotStyle):
-    __slots__ = ["_cm"]
-    CLIP_GRADIENT_AT_WEIGHT = 0.1  # clip color gradients for weights=10
-
-    def __init__(self, ax: Axes):
-        super().__init__(ax)
-        self._cm = cm.get_cmap(name="plasma", lut=256)
 
     def draw_leaf_labels(self, labels: Sequence[str]) -> None:
         y_axis = self._ax.yaxis
         y_axis.set_ticks(ticks=range(len(labels)))
         y_axis.set_ticklabels(ticklabels=labels)
 
+    def color(self, weight) -> RgbaColor:
+        return self._cm(
+            0
+            if weight <= self._min_weight
+            else 1 - math.log(weight) / math.log(self._min_weight)
+        )
+
+
+class LineStyle(MatplotStyle):
     def draw_link_leg(
         self, bottom: float, top: float, first_leaf: int, n_leaves: int, weight: float
     ) -> None:
         line_y = first_leaf + (n_leaves - 1) / 2
-        self._draw_line(
-            x1=bottom, x2=top, y1=line_y, y2=line_y, color=self.color(weight)
-        )
+        self._draw_line(x1=bottom, x2=top, y1=line_y, y2=line_y, weight=weight)
 
     def draw_link_connector(
         self,
@@ -62,7 +71,7 @@ class LineStyle(MatplotStyle):
             x2=bottom,
             y1=first_leaf + (n_leaves_left - 1) / 2,
             y2=first_leaf + n_leaves_left + (n_leaves_right - 1) / 2,
-            color=self.color(weight),
+            weight=weight,
         )
 
         self.draw_link_leg(
@@ -73,5 +82,44 @@ class LineStyle(MatplotStyle):
             weight=weight,
         )
 
-    def color(self, weight) -> RgbaColor:
-        return self._cm(weight / LineStyle.CLIP_GRADIENT_AT_WEIGHT)
+    def _draw_line(
+        self, x1: float, x2: float, y1: float, y2: float, weight: float
+    ) -> None:
+        self._ax.plot((x1, x2), (y1, y2), color=self.color(weight))
+
+
+class FeatMapStyle(MatplotStyle):
+    def draw_link_leg(
+        self, bottom: float, top: float, first_leaf: int, n_leaves: int, weight: float
+    ) -> None:
+        line_y = first_leaf + (n_leaves - 1) / 2
+        self._draw_hbar(x=bottom, w=top - bottom, y=line_y, h=1, weight=weight)
+
+    def draw_link_connector(
+        self,
+        bottom: float,
+        top: float,
+        first_leaf: int,
+        n_leaves_left: int,
+        n_leaves_right,
+        weight: float,
+    ) -> None:
+        self._draw_hbar(
+            x=bottom,
+            w=top - bottom,
+            y=first_leaf,
+            h=n_leaves_left + n_leaves_right,
+            weight=weight,
+        )
+
+    def _draw_hbar(self, x: float, y: float, w: float, h: float, weight: float) -> None:
+        self._ax.barh(
+            y=[y - 0.5],
+            width=[w],
+            height=[h],
+            left=[x],
+            align="edge",
+            color=self.color(weight),
+            edgecolor="white",
+            linewidth=1,
+        )
