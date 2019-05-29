6b503bdcfefe23fa2e92de4fca4fda463d3028b3
Ittner Jan <ittner.jan@bcg.com>
2019-05-29 20:46:17 +0200

    add named parameters to BorutaDF constructor

--- src/yieldengine/preprocessing/selection.py
+++ src/yieldengine/preprocessing/selection.py
@@ -5,6 +5,30 @@ from yieldengine.transform import ColumnPreservingTransformer, NumpyOnlyTransfor
 
 
 class BorutaDF(NumpyOnlyTransformer[BorutaPy], ColumnPreservingTransformer[BorutaPy]):
+    def __init__(
+        self,
+        estimator,
+        n_estimators=1000,
+        perc=100,
+        alpha=0.05,
+        two_step=True,
+        max_iter=100,
+        random_state=None,
+        verbose=0,
+        **kwargs
+    ):
+        super().__init__(
+            estimator=estimator,
+            n_estimators=n_estimators,
+            perc=perc,
+            alpha=alpha,
+            two_step=two_step,
+            max_iter=max_iter,
+            random_state=random_state,
+            verbose=verbose,
+            **kwargs
+        )
+
     @classmethod
     def _make_base_transformer(cls, **kwargs) -> BorutaPy:
         return BorutaPy(**kwargs)
