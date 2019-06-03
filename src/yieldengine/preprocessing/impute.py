566e85c863c72c89f2f341774fc397b404592d58
Ittner Jan <ittner.jan@bcg.com>
2019-06-03 14:24:30 +0200

    implement new MissingIndicatorDF transformer

--- src/yieldengine/preprocessing/impute.py
+++ src/yieldengine/preprocessing/impute.py
@@ -2,9 +2,9 @@ import logging
 
 import numpy as np
 import pandas as pd
-from sklearn.impute import SimpleImputer
+from sklearn.impute import SimpleImputer, MissingIndicator
 
-from yieldengine.transform import ColumnPreservingTransformer
+from yieldengine.transform import ColumnPreservingTransformer, DataFrameTransformer
 
 log = logging.getLogger(__name__)
 
@@ -26,3 +26,32 @@ class SimpleImputerDF(ColumnPreservingTransformer[SimpleImputer]):
                 x is None or (isinstance(x, float) and np.isnan(x)) for x in stats
             ]
         return self.columns_in.delete(np.argwhere(nan_mask))
+
+
+class MissingIndicatorDF(DataFrameTransformer[MissingIndicator]):
+    def __init__(
+        self,
+        missing_values=np.nan,
+        features="missing-only",
+        sparse="auto",
+        error_on_new=True,
+        **kwargs,
+    ):
+        super().__init__(
+            missing_values=missing_values,
+            features=features,
+            sparse=sparse,
+            error_on_new=error_on_new,
+            **kwargs,
+        )
+
+    @classmethod
+    def _make_base_transformer(cls, **kwargs) -> MissingIndicator:
+        return MissingIndicator(**kwargs)
+
+    def _get_columns_original(self) -> pd.Series:
+        columns_original: np.ndarray = self.columns_in[
+            self.base_transformer.features_
+        ].values
+        columns_out = pd.Index([f"{name}__missing" for name in columns_original])
+        return pd.Series(index=columns_out, data=columns_original)
