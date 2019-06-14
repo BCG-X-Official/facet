6d2d3345544e8f7f070204463158e7348a3b9399
Ittner Jan <ittner.jan@bcg.com>
2019-06-14 07:53:30 +0200

    update inheritance of abstract DataFrame... classes

--- src/yieldengine/df/predict.py
+++ src/yieldengine/df/predict.py
@@ -1,5 +1,5 @@
 import logging
-from abc import ABC
+from abc import ABCMeta
 from typing import Any, Generic, Optional, TypeVar, Union
 
 import pandas as pd
@@ -12,7 +12,9 @@ log = logging.getLogger(__name__)
 _BasePredictor = TypeVar("_BasePredictor", bound=Union[RegressorMixin, ClassifierMixin])
 
 
-class DataFramePredictor(DataFrameEstimator, ABC, Generic[_BasePredictor]):
+class DataFramePredictor(
+    Generic[_BasePredictor], DataFrameEstimator[_BasePredictor], metaclass=ABCMeta
+):
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
 
@@ -20,12 +22,14 @@ class DataFramePredictor(DataFrameEstimator, ABC, Generic[_BasePredictor]):
     def predict(self, X: pd.DataFrame, **predict_params):
         self._check_parameter_types(X, None)
 
+        # noinspection PyUnresolvedReferences
         return self.base_estimator.predict(X, **predict_params)
 
     # noinspection PyPep8Naming
     def fit_predict(self, X: pd.DataFrame, y: pd.Series, **fit_params):
         self._check_parameter_types(X, y)
 
+        # noinspection PyUnresolvedReferences
         result = self.base_estimator.fit_predict(X, y, **fit_params)
 
         self._post_fit(X, y, **fit_params)
@@ -36,18 +40,21 @@ class DataFramePredictor(DataFrameEstimator, ABC, Generic[_BasePredictor]):
     def predict_proba(self, X: pd.DataFrame):
         self._check_parameter_types(X, None)
 
+        # noinspection PyUnresolvedReferences
         return self.base_estimator.predict_proba(X)
 
     # noinspection PyPep8Naming
     def decision_function(self, X: pd.DataFrame):
         self._check_parameter_types(X, None)
 
+        # noinspection PyUnresolvedReferences
         return self.base_estimator.decision_function(X)
 
     # noinspection PyPep8Naming
     def predict_log_proba(self, X: pd.DataFrame):
         self._check_parameter_types(X, None)
 
+        # noinspection PyUnresolvedReferences
         return self.base_estimator.predict_log_proba(X)
 
     # noinspection PyPep8Naming
