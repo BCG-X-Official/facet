7a054334b99186db92502e5a214349e8aca8c7f9
Joerg Schneider <schneider.joerg@bcg.com>
2019-06-24 14:41:13 +0200

    add another, conciser method to construct constant-column-transfomers

--- src/yieldengine/preprocessing/__init__.py
+++ src/yieldengine/preprocessing/__init__.py
@@ -14,62 +14,34 @@ from sklearn.preprocessing import (
 )
 
 from yieldengine.df.transform import (
-    _BaseTransformer,
     ColumnPreservingTransformer,
-    constant_column_transformer,
-    ConstantColumnTransformer,
+    make_constant_column_transformer_class,
 )
 
 log = logging.getLogger(__name__)
 
 
-@constant_column_transformer(source_transformer=MinMaxScaler)
-class MinMaxScalerDF(ConstantColumnTransformer[MinMaxScaler]):
-    @classmethod
-    def _make_base_transformer(cls, **kwargs) -> _BaseTransformer:
-        pass
-
-
-@constant_column_transformer(source_transformer=StandardScaler)
-class StandardScalerDF(ConstantColumnTransformer[StandardScaler]):
-    @classmethod
-    def _make_base_transformer(cls, **kwargs) -> _BaseTransformer:
-        pass
-
-
-@constant_column_transformer(source_transformer=MaxAbsScaler)
-class MaxAbsScalerDF(ConstantColumnTransformer[MaxAbsScaler]):
-    @classmethod
-    def _make_base_transformer(cls, **kwargs) -> _BaseTransformer:
-        pass
-
-
-@constant_column_transformer(source_transformer=RobustScaler)
-class RobustScalerDF(ConstantColumnTransformer[RobustScaler]):
-    @classmethod
-    def _make_base_transformer(cls, **kwargs) -> _BaseTransformer:
-        pass
-
-
-@constant_column_transformer(source_transformer=PowerTransformer)
-class PowerTransformerDF(ConstantColumnTransformer[PowerTransformer]):
-    @classmethod
-    def _make_base_transformer(cls, **kwargs) -> _BaseTransformer:
-        pass
-
-
-@constant_column_transformer(source_transformer=QuantileTransformer)
-class QuantileTransformerDF(ConstantColumnTransformer[QuantileTransformer]):
-    @classmethod
-    def _make_base_transformer(cls, **kwargs) -> _BaseTransformer:
-        pass
-
-
-@constant_column_transformer(source_transformer=Normalizer)
-class NormalizerDF(ConstantColumnTransformer[Normalizer]):
-    @classmethod
-    def _make_base_transformer(cls, **kwargs) -> _BaseTransformer:
-        pass
+MaxAbsScalerDF = make_constant_column_transformer_class(
+    source_transformer=MaxAbsScaler, class_name="MaxAbsScalerDF"
+)
+MinMaxScalerDF = make_constant_column_transformer_class(
+    source_transformer=MinMaxScaler, class_name="MinMaxScalerDF"
+)
+NormalizerDF = make_constant_column_transformer_class(
+    source_transformer=Normalizer, class_name="NormalizerDF"
+)
+PowerTransformerDF = make_constant_column_transformer_class(
+    source_transformer=PowerTransformer, class_name="PowerTransformerDF"
+)
+QuantileTransformerDF = make_constant_column_transformer_class(
+    source_transformer=QuantileTransformer, class_name="QuantileTransformerDF"
+)
+RobustScalerDF = make_constant_column_transformer_class(
+    source_transformer=RobustScaler, class_name="RobustScalerDF"
+)
+StandardScalerDF = make_constant_column_transformer_class(
+    source_transformer=StandardScaler, class_name="StandardScalerDF"
+)
 
 
 class FunctionTransformerDF(ColumnPreservingTransformer[FunctionTransformer]):
