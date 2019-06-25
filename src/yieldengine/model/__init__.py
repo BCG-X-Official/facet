6bf2a2a8ea5393c4ff45fd7266800c3f003dd87a
Martin Florent <martin.florent@bcg.com>
2019-06-25 13:40:09 +0200

    extend docstrings

--- src/yieldengine/model/__init__.py
+++ src/yieldengine/model/__init__.py
@@ -1,3 +1,10 @@
+# coding=utf-8
+"""This module contains classes for:
+ - feature selection
+ - model creation
+ - cross validation
+ - model inspection
+ """
 from typing import *
 
 from sklearn import clone
@@ -36,6 +43,10 @@ class Model:
         self._preprocessing = preprocessing
 
     def pipeline(self) -> PipelineDF:
+        """The underlying `PipelineDF`.
+        
+        It has two steps: `preprocessing` and `estimator`.
+        """
         return PipelineDF(
             steps=[
                 (Model.STEP_PREPROCESSING, self.preprocessing),
@@ -45,13 +56,20 @@ class Model:
 
     @property
     def preprocessing(self) -> DataFrameTransformer:
+        """The `DataFrameTransformer` used as preprocessing step in the pipeline."""
         return self._preprocessing
 
     @property
     def estimator(self) -> BaseEstimator:
+        """The `DataFrameTransformer` used as estimator in the pipeline."""
         return self._estimator
 
     def clone(self, parameters: Optional[Dict[str, Any]] = None) -> "Model":
+        """Returns a clone Model.
+
+        :param parameters: parameters used to reset the parameters of the pipeline
+        :return: a cloned `Model`
+        """
         estimator = clone(self._estimator)
         preprocessing = self._preprocessing
         if preprocessing is not None:
