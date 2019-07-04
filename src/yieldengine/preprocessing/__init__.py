51b5ba06588d8044d07ea9e7cb7313692dbbeed8
Martin Florent <martin.florent@bcg.com>
2019-07-04 09:21:23 +0200

    Add docstrings to preprocessing.__init__

--- src/yieldengine/preprocessing/__init__.py
+++ src/yieldengine/preprocessing/__init__.py
@@ -8,6 +8,8 @@ log = logging.getLogger(__name__)
 
 
 class FunctionTransformerDF(ConstantColumnTransformer[FunctionTransformer]):
+    """Wrapper around sklearn ```FuctionTransformer``` that returns a DataFrame
+    with correct row and column indices."""
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
 
