71b44881561627e96177ea83a46e15e9fd045fa1
Joerg Schneider <schneider.joerg@bcg.com>
2019-07-05 10:47:00 +0200

    add DiscreteValuePartitioning

--- src/yieldengine/sampling.py
+++ src/yieldengine/sampling.py
@@ -42,6 +42,22 @@ class ValuePartitioning(ABC, Generic[ValueType]):
     def partition_width(self) -> ValueType:
         pass
 
+    @staticmethod
+    def set_bounds(
+        lower_bound: Optional[ValueType],
+        upper_bound: Optional[ValueType],
+        values: ListLike[ValueType],
+    ) -> Tuple[ValueType, ValueType]:
+
+        if lower_bound is None:
+            lower_bound = np.min(values)
+        if upper_bound is None:
+            upper_bound = np.max(values)
+        if upper_bound < lower_bound:
+            raise ValueError("arg lower_bound > arg upper_bound")
+
+        return lower_bound, upper_bound
+
 
 class ContinuousValuePartitioning(ValuePartitioning[np.float]):
     def __init__(
@@ -52,12 +68,10 @@ class ContinuousValuePartitioning(ValuePartitioning[np.float]):
         upper_bound: Optional[np.float] = None,
     ) -> None:
         super().__init__()
-        if lower_bound is None:
-            lower_bound = np.min(values)
-        if upper_bound is None:
-            upper_bound = np.max(values)
-        if upper_bound < lower_bound:
-            raise ValueError("arg lower_bound > arg upper_bound")
+
+        lower_bound, upper_bound = ContinuousValuePartitioning.set_bounds(
+            lower_bound, upper_bound, values
+        )
 
         # calculate the step count based on the maximum number of partitions,
         # rounded to the next-largest rounded value ending in 1, 2, or 5
@@ -71,7 +85,7 @@ class ContinuousValuePartitioning(ValuePartitioning[np.float]):
 
         self._values = values
 
-    def partitions(self) -> Sequence[ValueType]:
+    def partitions(self) -> Sequence[np.float]:
         return [idx * self._step for idx in range(0, self.n_partitions())]
 
     def frequencies(self) -> Iterable[int]:
@@ -90,7 +104,59 @@ class ContinuousValuePartitioning(ValuePartitioning[np.float]):
     def n_partitions(self) -> int:
         return int((self._last_partition - self._first_partition) / self._step) + 1
 
-    def partition_width(self) -> ValueType:
+    def partition_width(self) -> np.float:
+        return self._step
+
+
+class DiscreteValuePartitioning(ValuePartitioning[np.int]):
+    def __init__(
+        self,
+        values: ListLike[np.int],
+        max_partitions: int = 20,
+        lower_bound: Optional[np.int] = None,
+        upper_bound: Optional[np.int] = None,
+    ) -> None:
+        super().__init__()
+
+        lower_bound, upper_bound = DiscreteValuePartitioning.set_bounds(
+            lower_bound, upper_bound, values
+        )
+
+        # calculate the step count based on the maximum number of partitions,
+        # rounded to the next-largest rounded value ending in 1, 2, or 5
+        step = int(ceil_step((upper_bound - lower_bound) / (max_partitions - 1)))
+        self._step = step
+
+        # calculate centre values of the first and last partition;
+        # both are rounded to multiples of the step size
+        self._first_partition = int(math.floor((lower_bound + step / 2) / step) * step)
+        self._last_partition = int(math.ceil((upper_bound - step / 2) / step) * step)
+
+        self._values = values
+
+    def partitions(self) -> Sequence[np.int]:
+        return [idx * self._step for idx in range(0, self.n_partitions())]
+
+    def frequencies(self) -> Iterable[int]:
+        frequencies = []
+
+        for p_center in self.partitions():
+            p_l_bound = math.floor(p_center - self.partition_width() / 2)
+            p_r_bound = math.ceil(p_center + self.partition_width() / 2)
+
+            frequencies.append(
+                sum([1 for val in self._values if val >= p_l_bound and val < p_r_bound])
+            )
+
+        return frequencies
+
+    def n_partitions(self) -> int:
+        return (
+            int(math.ceil((self._last_partition - self._first_partition) / self._step))
+            + 1
+        )
+
+    def partition_width(self) -> np.int:
         return self._step
 
 
