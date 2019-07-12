6661aff09d9cbe2b6ebaa70814d95d16db26d600
Joerg Schneider <schneider.joerg@bcg.com>
2019-07-12 17:52:56 +0200

    WIP: add first extensions + test-case for classification use case

--- src/tests/model/test_inspection.py
+++ src/tests/model/test_inspection.py
@@ -7,12 +7,13 @@ from lightgbm.sklearn import LGBMRegressor
 from shap import KernelExplainer, TreeExplainer
 from shap.explainers.explainer import Explainer
 from sklearn.base import BaseEstimator
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import BaseCrossValidator, RepeatedKFold
 from sklearn.svm import SVR
 
 from yieldengine import Sample
 from yieldengine.df.transform import DataFrameTransformer
-from yieldengine.model import RegressionModel
+from yieldengine.model import ClassificationModel, RegressionModel
 from yieldengine.model.inspection import ModelInspector
 from yieldengine.model.prediction import ModelFitCV
 from yieldengine.model.selection import (
@@ -196,3 +197,92 @@ def test_model_inspection_with_encoding(
 
         mi2 = ModelInspector(model_fit=model_fit, explainer_factory=ef)
         mi2.shap_matrix()
+
+
+def test_model_inspection_classifier(available_cpus: int, iris_sample: Sample) -> None:
+    warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+    warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
+    warnings.filterwarnings("ignore", message="You are accessing a training score")
+
+    # define a CV:
+    # noinspection PyTypeChecker
+    test_cv: BaseCrossValidator = RepeatedKFold(
+        n_splits=K_FOLDS, n_repeats=N_SPLITS // K_FOLDS, random_state=42
+    )
+
+    # define parameters and models
+    models = [
+        ModelGrid(
+            model=ClassificationModel(
+                estimator=RandomForestClassifier(), preprocessing=None
+            ),
+            estimator_parameters={"n_estimators": [50, 80]},
+        )
+    ]
+
+    test_sample: Sample = iris_sample
+
+    model_ranker: ModelRanker = ModelRanker(
+        grids=models, cv=test_cv, scoring="f1_macro"
+    )
+
+    model_ranking: Sequence[ModelEvaluation] = model_ranker.run(
+        test_sample, n_jobs=available_cpus
+    )
+
+    log.debug(f"\n{summary_report(model_ranking[:10])}")
+
+    # consider: model_with_type(...) function for ModelRanking
+    model_evaluation = model_ranking[0]
+
+    model_fit = ModelFitCV(
+        model=model_evaluation.model,
+        cv=test_cv,
+        sample=test_sample,
+        n_jobs=available_cpus,
+    )
+
+    # test predictions_for_all_samples
+    predictions_df: pd.DataFrame = model_fit.predictions_for_all_splits()
+    assert ModelFitCV.F_PREDICTION in predictions_df.columns
+    assert ModelFitCV.F_TARGET in predictions_df.columns
+
+    # check number of split ids
+    assert (
+        predictions_df.index.get_level_values(level=ModelFitCV.F_SPLIT_ID).nunique()
+        == N_SPLITS
+    )
+
+    # check correct number of rows
+    allowed_variance = 0.01
+    assert (
+        (len(test_sample) * (TEST_RATIO - allowed_variance) * N_SPLITS)
+        <= len(predictions_df)
+        <= (len(test_sample) * (TEST_RATIO + allowed_variance) * N_SPLITS)
+    )
+
+    model_inspector = ModelInspector(model_fit=model_fit)
+    # make and check shap value matrix
+    shap_matrix = model_inspector.shap_matrix()
+
+    # the length of rows in shap_matrix should be equal to the unique observation
+    # indices we have had in the predictions_df
+    assert len(shap_matrix) == len(test_sample)
+
+    # correlated shap matrix: feature dependencies
+    corr_matrix: pd.DataFrame = model_inspector.feature_dependency_matrix()
+    log.info(corr_matrix)
+    # check number of rows
+    assert len(corr_matrix) == len(test_sample.feature_names) - 1
+    assert len(corr_matrix.columns) == len(test_sample.feature_names) - 1
+
+    # check correlation values
+    for c in corr_matrix.columns:
+        assert (
+            -1.0
+            <= corr_matrix.fillna(0).loc[:, c].min()
+            <= corr_matrix.fillna(0).loc[:, c].max()
+            <= 1.0
+        )
+
+    linkage_tree = model_inspector.cluster_dependent_features()
