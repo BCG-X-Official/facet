deca498e33c58347913d66a21788cf60e2e29863
Joerg Schneider <schneider.joerg@bcg.com>
2019-07-12 13:43:26 +0200

    change from Model to RegressionModel where applicable (mostly tests)

--- src/tests/model/test_init.py
+++ src/tests/model/test_init.py
@@ -3,14 +3,16 @@ from lightgbm import LGBMRegressor
 from sklearn.preprocessing import OneHotEncoder
 
 from tests.model import make_simple_transformer
-from yieldengine.model import Model
+from yieldengine.model import RegressionModel
 
 
 def test_model() -> None:
 
-    model = Model(estimator=LGBMRegressor(), preprocessing=make_simple_transformer())
+    model = RegressionModel(
+        estimator=LGBMRegressor(), preprocessing=make_simple_transformer()
+    )
 
     # test-type check within constructor:
     with pytest.raises(ValueError):
         # noinspection PyTypeChecker
-        Model(estimator=LGBMRegressor(), preprocessing=OneHotEncoder())
+        RegressionModel(estimator=LGBMRegressor(), preprocessing=OneHotEncoder())
