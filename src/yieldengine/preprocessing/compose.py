2104bbda3c8c6965fa25bf0ada8681e530668b41
Martin Florent <martin.florent@bcg.com>
2019-07-16 20:18:27 +0200

    clean the docstrings according to the guideline

--- src/yieldengine/preprocessing/compose.py
+++ src/yieldengine/preprocessing/compose.py
@@ -1,6 +1,4 @@
-"""
-This module defines wrappers around the sklearn ```ColumnTransformer```.
-"""
+"""Wrapper around sklearn `ColumnTransformer` compatible with dataframes."""
 
 import logging
 from functools import reduce
@@ -17,8 +15,14 @@ __all__ = ["ColumnTransformerDF"]
 
 
 class ColumnTransformerDF(DataFrameTransformer[ColumnTransformer]):
-    """Wrapper around sklearn ```ColumnTransformer``` that returns a DataFrame
-    with correct row and column indices."""
+    """Wrap sklearn ```ColumnTransformer``` and return a DataFrame.
+
+    Like a sklearn `ColumnTransformer`, self has a `transformers` parameter (None by
+    default) which is a list of tuple of the form (name, transformer, column(s)),
+    but here all the transformers must be of type
+    :class:`~yieldengine.df.transform.DataFrameTransformer`.
+    """
+
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
         # noinspection PyTypeChecker
@@ -50,12 +54,11 @@ class ColumnTransformerDF(DataFrameTransformer[ColumnTransformer]):
         return ColumnTransformer(**kwargs)
 
     def _get_columns_original(self) -> pd.Series:
-        """
-        :return: a list of the original features from which the output of this
-        transformer is derived. The list is aligned with the columns returned by
-        method :meth" `output columns` and this has the same length
-        """
+        """Return the series mapping output column names to original columns names.
 
+        :return: the series with index the column names of the output dataframe and
+        values the corresponding input column names.
+        """
         return reduce(
             lambda x, y: x.append(y),
             (
