e4bd8fda55bb0d7f6e1f795a6cbd345df0fa1079
Ittner Jan <ittner.jan@bcg.com>
2019-07-22 18:53:46 +0200

    move dendrogram visualization to module viz; factor out generic drawer and style classes

--- src/yieldengine/viz/dendrogram/_style.py
+++ src/yieldengine/viz/dendrogram/_style.py
@@ -4,7 +4,7 @@ Dendrogram styles.
 The dendrogram styles are given as a parameter to a
 :class:`~yieldengine.dendrogram.DendrogramDrawer` and determine the style of the plot.
 
-:class:`MatplotStyle` is a an abstract base class for styles using matplotlib.
+:class:`DendrogramMatplotStyle` is a an abstract base class for styles using matplotlib.
 
 :class:`~LineStyle` renders dendrogram trees in the classical style as a line drawing.
 
@@ -13,10 +13,10 @@ for better visibility of feature importance.
 """
 
 import logging
-from abc import ABC
-from typing import *
-
 import math
+from abc import ABC, abstractmethod
+from typing import Sequence
+
 import matplotlib.text as mt
 from matplotlib import cm
 from matplotlib.axes import Axes
@@ -24,12 +24,9 @@ from matplotlib.colorbar import ColorbarBase, make_axes
 from matplotlib.colors import LogNorm
 from matplotlib.ticker import Formatter
 
-from yieldengine.dendrogram import DendrogramStyle
+from yieldengine.viz import ChartStyle, MatplotStyle, RgbaColor
 
 log = logging.getLogger(__name__)
-
-RgbaColor = Tuple[float, float, float, float]
-
 
 _COLOR_BLACK = "black"
 _COLOR_WHITE = "white"
@@ -42,7 +39,61 @@ class _PercentageFormatter(Formatter):
         return f"{x * 100.0:.0f}%"
 
 
-class MatplotStyle(DendrogramStyle, ABC):
+class DendrogramStyle(ChartStyle, ABC):
+    """
+    Base class for dendrogram drawing styles.
+
+    Implementations must define `draw_leaf_labels`, `draw_title`, `draw_link_leg` \
+    and `draw_link_connector`.
+    """
+
+    @abstractmethod
+    def draw_leaf_labels(self, labels: Sequence[str]) -> None:
+        """Render the labels for all leaves.
+
+        :param labels: labels of the leaves
+        """
+        pass
+
+    @abstractmethod
+    def draw_link_leg(
+        self, bottom: float, top: float, first_leaf: int, n_leaves: int, weight: float
+    ) -> None:
+        """
+        Draw a leaf of the linkage tree.
+
+        :param bottom: the clustering level (i.e. similarity) of the child nodes
+        :param top: the clustering level (i.e. similarity) of the parent node
+        :param first_leaf: the index of the first leaf in the tree
+        :param n_leaves: number of leaves under consideration, always set to 1
+        :param weight: the weight of the parent node
+        """
+        pass
+
+    @abstractmethod
+    def draw_link_connector(
+        self,
+        bottom: float,
+        top: float,
+        first_leaf: int,
+        n_leaves_left: int,
+        n_leaves_right: int,
+        weight: float,
+    ) -> None:
+        """
+        Draw a connector between two child nodes and their parent node.
+
+        :param bottom: the clustering level (i.e. similarity) of the child nodes
+        :param top: the clustering level (i.e. similarity) of the parent node
+        :param first_leaf: the index of the first leaf in the tree
+        :param n_leaves_left: the number of leaves in the left sub-tree
+        :param n_leaves_right: the number of leaves in the right sub-tree
+        :param weight: the weight of the parent node
+        """
+        pass
+
+
+class DendrogramMatplotStyle(DendrogramStyle, MatplotStyle, ABC):
     """
     Base class for Matplotlib styles for dendrogram.
 
@@ -55,13 +106,10 @@ class MatplotStyle(DendrogramStyle, ABC):
                        greater than 0 and smaller than 1 (default: 0.01)
     """
 
-    __slots__ = ["_ax", "_cm", "_min_weight"]
-
     _PERCENTAGE_FORMATTER = _PercentageFormatter()
 
     def __init__(self, ax: Axes, min_weight: float = 0.01) -> None:
-        super().__init__()
-        self._ax = ax
+        super().__init__(ax=ax)
 
         if min_weight >= 1.0 or min_weight <= 0.0:
             raise ValueError("arg min_weight must be > 0.0 and < 1.0")
@@ -80,16 +128,16 @@ class MatplotStyle(DendrogramStyle, ABC):
             orientation="vertical",
         )
 
-        cax.yaxis.set_minor_formatter(MatplotStyle._PERCENTAGE_FORMATTER)
-        cax.yaxis.set_major_formatter(MatplotStyle._PERCENTAGE_FORMATTER)
+        cax.yaxis.set_minor_formatter(DendrogramMatplotStyle._PERCENTAGE_FORMATTER)
+        cax.yaxis.set_major_formatter(DendrogramMatplotStyle._PERCENTAGE_FORMATTER)
 
     def draw_title(self, title: str) -> None:
         """Draw the title of the dendrogram."""
-        self._ax.set_title(label=title)
+        self.ax.set_title(label=title)
 
     def draw_leaf_labels(self, labels: Sequence[str]) -> None:
         """Draw leaf labels on the dendrogram."""
-        y_axis = self._ax.yaxis
+        y_axis = self.ax.yaxis
         y_axis.set_ticks(ticks=range(len(labels)))
         y_axis.set_ticklabels(ticklabels=labels)
 
@@ -107,7 +155,7 @@ class MatplotStyle(DendrogramStyle, ABC):
         )
 
 
-class LineStyle(MatplotStyle):
+class LineStyle(DendrogramMatplotStyle):
     """The classical dendrogram style, as a coloured tree diagram."""
 
     def draw_link_leg(
@@ -169,10 +217,10 @@ class LineStyle(MatplotStyle):
     def _draw_line(
         self, x1: float, x2: float, y1: float, y2: float, weight: float
     ) -> None:
-        self._ax.plot((x1, x2), (y1, y2), color=self.color(weight))
+        self.ax.plot((x1, x2), (y1, y2), color=self.color(weight))
 
 
-class FeatMapStyle(MatplotStyle):
+class FeatMapStyle(DendrogramMatplotStyle):
     """
     Plot dendrograms with a heat map style.
 
@@ -247,7 +295,7 @@ class FeatMapStyle(MatplotStyle):
         """
         fill_color = self.color(weight)
 
-        self._ax.barh(
+        self.ax.barh(
             y=[y - 0.5],
             width=[w],
             height=[h],
@@ -265,14 +313,14 @@ class FeatMapStyle(MatplotStyle):
             else f"{weight_percent:.3g}%"
         )
         fig = self._figure
-        (x0, _), (x1, _) = self._ax.transData.inverted().transform(
+        (x0, _), (x1, _) = self.ax.transData.inverted().transform(
             mt.Text(0, 0, label, figure=fig).get_window_extent(self._renderer)
         )
 
         if abs(x1 - x0) <= w:
             fill_luminance = sum(fill_color[:3]) / 3
             text_color = _COLOR_WHITE if fill_luminance < 0.5 else _COLOR_BLACK
-            self._ax.text(
+            self.ax.text(
                 x + w / 2,
                 y + (h - 1) / 2,
                 label,
