dc2404c3b0b8bb4be32498c08ec9f549ef979b22
Martin Florent <martin.florent@bcg.com>
2019-07-23 13:33:56 +0200

    add file structure fot the MVC simulation drawing pattern

---
+++ src/gamma/viz/simulation/_style.py
@@ -0,0 +1,13 @@
+"""
+Simulation drawing styles
+"""
+
+import logging
+
+from gamma.viz import MatplotStyle
+
+log = logging.getLogger(__name__)
+
+
+class SimulationMatplotStyle(MatplotStyle):
+    pass
