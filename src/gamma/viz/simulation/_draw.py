4a583fdc41da8fdde81160f4b2157d1611726ca8
Martin Florent <martin.florent@bcg.com>
2019-07-24 23:01:27 +0200

    change the MVC simulation pattern: - SimulationDrawer is independent from matplotlib - use UnivariateSimulation as Model in the MVC pattern

--- src/gamma/viz/simulation/_draw.py
+++ src/gamma/viz/simulation/_draw.py
@@ -8,20 +8,17 @@ this graph there is a histogram of the feature values.
 
 from typing import Optional
 
-import matplotlib.pyplot as plt
-from matplotlib.axes import Axes
-
 from gamma.viz import ChartDrawer
 from gamma.viz.simulation._simulation_data import SimulationData, T_RangePartitioning
 from gamma.viz.simulation._style import SimulationMatplotStyle
 from gamma.yieldengine.partition import NumericType
+from gamma.yieldengine.simulation import UnivariateSimulation
 
 
 class SimulationDrawer(ChartDrawer[SimulationData, SimulationMatplotStyle]):
     """
     Simulation drawer with high/low confidence intervals.
 
-    :param simulation_data: data used for the simulation
     :param style: drawing style for the uplift graph and the feature histogram
     :param title: title of the char
     :param histogram: if ``True`` (default) the feature histogram is plotted,
@@ -30,71 +27,74 @@ class SimulationDrawer(ChartDrawer[SimulationData, SimulationMatplotStyle]):
 
     def __init__(
         self,
-        simulation_data: SimulationData,
+        # simulation_data: SimulationData,
         style: SimulationMatplotStyle,
         title: Optional[str] = None,
         histogram: bool = True,
+        simulation: Optional[UnivariateSimulation] = None,
     ):
-        super().__init__(title=title, model=simulation_data, style=style)
-        self._simulation_data = simulation_data
+        super().__init__(title=title, model=simulation, style=style)
+        # self._simulation_data = simulation
         self._style = style
         self._title = title if title else "Simulation"
         self._histogram = histogram
+        self._simulation = simulation
 
     def _draw(self) -> None:
         # draw the simulation chart
+        self._style.initialize_chart(histogram=self._histogram)
+
+        self._draw_uplift_graph()
+
         if self._histogram:
-            fig, (ax1, ax2) = plt.subplots(nrows=2, figsize=(10, 10), sharex="all")
-            self._draw_uplift_axes(ax1)
-            self._draw_histogram_axes(ax2)
-        else:
-            fig, ax1 = plt.subplots(nrows=1, figsize=(10, 10))
-            self._draw_uplift_axes(ax1)
-        return None
+            self._draw_histogram()
 
-    def _draw_uplift_axes(self, ax: Axes) -> None:
-        # draw the uplift curves
-        self._style.ax = ax
+        self._draw_title()
 
-        line_low = self.style.draw_low_confidence_uplift(
-            *self._simulation_data.low_confidence_uplift
-        )
-
-        line_middle = self.style.draw_middle_confidence_uplift(
-            *self._simulation_data.middle_confidence_uplift
-        )
-
-        line_high = self.style.draw_high_confidence_uplift(
-            *self._simulation_data.high_confidence_uplift
-        )
-
-        handles = [line_high, line_middle, line_low]
-        labels = ["90th percentile", "Median", "10th percentile"]
-        self._style.draw_uplift_legend(handles=handles, labels=labels)
-
-        xaxis_label = self._simulation_data.feature
-        x_ticks = self._simulation_data.partitioning.partitions()
-        yaxis_label = f"Predicted mean uplift ({self._simulation_data.target_name})"
-        self.style.draw_uplift_axis(
-            xaxis_label=xaxis_label, yaxis_label=yaxis_label, x_ticks=x_ticks
-        )
-
-        self._style.draw_null_uplift_line()
-
-        self._style.set_spins()
-
+    def _draw_title(self) -> None:
         self._style.draw_title(self._title)
-        return None
 
-    def _draw_histogram_axes(self, ax: Axes) -> None:
+    def _draw_uplift_graph(self) -> None:
+        # draw the graph with the uplift curves
+        feature = self._simulation.feature
+        target_name = self._simulation.target_name
+        feature_values = self._simulation.feature_values
+        median_uplift = self._simulation.median_uplift
+        low_percentile_uplift = self._simulation.low_percentile_uplift
+        high_percentile_uplift = self._simulation.high_percentile_uplift
+        low_percentile = self._simulation.low_percentile
+        high_percentile = self._simulation.high_percentile
+
+        self._style.draw_uplift_graph(
+            feature=feature,
+            target_name=target_name,
+            feature_values=feature_values,
+            median_uplift=median_uplift,
+            low_percentile_uplift=low_percentile_uplift,
+            high_percentile_uplift=high_percentile_uplift,
+            low_percentile=low_percentile,
+            high_percentile=high_percentile,
+        )
+
+    def _draw_histogram(self) -> None:
         # draw the histogram of the simulation values
-        self._style.ax = ax
 
-        x = self._simulation_data.partitioning.partitions()
-        height = self._simulation_data.partitioning.frequencies()
-        partition_width = self._simulation_data.partitioning.partition_width
+        feature_values = self._simulation.feature_values
+        feature_frequencies = self._simulation.feature_frequencies
+        partition_width = self._simulation.partition_width
 
-        self._style.draw_histogram(x=x, height=height, width=partition_width)
+        self._style.draw_histogram(
+            feature_values=feature_values,
+            feature_frequencies=feature_frequencies,
+            partition_width=partition_width,
+        )
+        # self._style.ax = ax
+        #
+        # x = self._simulation_data.partitioning.partitions()
+        # height = self._simulation_data.partitioning.frequencies()
+        # partition_width = self._simulation_data.partitioning.partition_width
+        #
+        # self._style.draw_histogram(x=x, height=height, width=partition_width)
         return None
 
     def set_feature(self, feature: str) -> None:
@@ -103,7 +103,7 @@ class SimulationDrawer(ChartDrawer[SimulationData, SimulationMatplotStyle]):
 
         :param feature: the new feature to use for the simulation
         """
-        self._simulation_data.feature = feature
+        self._simulation.feature = feature
         return None
 
     def update_simulation(
@@ -123,7 +123,7 @@ class SimulationDrawer(ChartDrawer[SimulationData, SimulationMatplotStyle]):
         :param upper_bound: maximum value for the simulated feature values
         :param partition_type: class to use for the partitioning
         """
-        self._simulation_data.update(
+        self._simulation.update(
             feature=feature,
             max_partitions=max_partitions,
             lower_bound=lower_bound,
