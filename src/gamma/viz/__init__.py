4b0ba6716deb8e3adeb95c1deee993e434ff700d
Ittner Jan <ittner.jan@bcg.com>
2019-07-25 10:00:57 +0200

    add text style for dendrograms

--- src/gamma/viz/__init__.py
+++ src/gamma/viz/__init__.py
@@ -1,8 +1,9 @@
 """
 MVC-based classes for drawing charts.
 """
+import sys
 from abc import ABC, abstractmethod
-from typing import Generic, Tuple, TypeVar
+from typing import Generic, TextIO, Tuple, TypeVar
 
 from matplotlib.axes import Axes
 
@@ -34,8 +35,9 @@ class ChartDrawer(Generic[T_Model, T_Style], ABC):
         return self._style
 
     def draw(self) -> None:
-        self.style.draw_title(self._title)
+        self.style.drawing_start(self._title)
         self._draw()
+        self.style.drawing_finalize()
 
     @abstractmethod
     def _draw(self) -> None:
@@ -49,10 +51,18 @@ class ChartDrawer(Generic[T_Model, T_Style], ABC):
 
 class ChartStyle(ABC):
     @abstractmethod
-    def draw_title(self, title: str) -> None:
+    def drawing_start(self, title: str) -> None:
         """
-        Draw the diagram title.
-        :title: the diagram title
+        Start drawing a new chart.
+        :title: the chart title
+        """
+        pass
+
+    def drawing_finalize(self) -> None:
+        """
+        Finalize the chart.
+
+        Does nothing by default, can optionally be overloaded
         """
         pass
 
@@ -64,7 +74,37 @@ class MatplotStyle(ChartStyle, ABC):
 
     @property
     def ax(self) -> Axes:
+        """
+        The matplot :class:`~matplotlib.axes.Axes` object to draw the chart in.
+        """
         return self._ax
+
+
+class TextStyle(ChartStyle, ABC):
+    """
+    Plain text drawing style.
+    :param width: the maximum width available to render the text, defaults to 80
+    :param out: the output stream this style instance writes to, or `stdout` if
+                `None` is passed (defaults to `None`)
+    """
+
+    def __init__(self, out: TextIO = None, width: int = 80) -> None:
+        self._out = sys.stdout if out is None else out
+        self._width = width
+
+    @property
+    def out(self) -> TextIO:
+        """
+        The output stream this style instance writes to.
+        """
+        return self._out
+
+    @property
+    def width(self) -> int:
+        """
+        The maximum width of the text to be produced.
+        """
+        return self._width
 
 
 #
