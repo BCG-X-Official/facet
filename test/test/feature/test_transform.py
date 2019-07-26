05c44001ddf767e764f53c41ac4bd74fc9b8ec6b
Martin Florent <martin.florent@bcg.com>
2019-07-26 14:57:26 +0200

    add a test for a boruta pipeline

--- test/test/feature/test_transform.py
+++ test/test/feature/test_transform.py
@@ -1,8 +1,35 @@
+import pandas as pd
+
 from gamma import Sample
 from gamma.sklearndf import TransformerDF
+from gamma.sklearndf.pipeline import PipelineDF
+from gamma.sklearndf.regression import RandomForestRegressorDF
+from gamma.sklearndf.transformation import SimpleImputerDF
+from gamma.sklearndf.transformation.extra import BorutaDF
 
 
 def test_column_transformer_df(
     sample: Sample, simple_preprocessor: TransformerDF
 ) -> None:
     simple_preprocessor.fit_transform(X=sample.features)
+
+
+def test_boruta_df(boston_df: pd.DataFrame, boston_target: str) -> None:
+    # test a pipeline with on the boston dataset
+
+    boruta_selector = PipelineDF(
+        steps=[
+            ("preprocess", SimpleImputerDF(strategy="median")),
+            (
+                "boruta",
+                BorutaDF(
+                    estimator=RandomForestRegressorDF(), n_estimators=10, max_iter=10
+                ),
+            ),
+        ]
+    )
+
+    x = boston_df.drop(columns=boston_target)
+    y = boston_df.loc[:, boston_target]
+
+    boruta_selector.fit(x, y)
