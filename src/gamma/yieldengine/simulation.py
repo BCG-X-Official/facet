1dcbf85e762fd060735352a714a30bdbed388865
Ittner Jan <ittner.jan@bcg.com>
2019-07-26 11:37:57 +0200

    streamlined simulator and simulation viz

--- src/gamma/yieldengine/simulation.py
+++ src/gamma/yieldengine/simulation.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 from gamma.model.prediction import PredictorFitCV
 from gamma.sklearndf.transformation import FunctionTransformerDF
-from gamma.yieldengine.partition import RangePartitioning, T_NumericValue
+from gamma.yieldengine.partition import Partitioning, T_Number
 
 
 class UnivariateSimulation:
@@ -19,67 +19,55 @@ class UnivariateSimulation:
 
     def __init__(
         self,
-        feature: str,
+        feature_name: str,
         target_name: str,
-        feature_values: Iterable[T_NumericValue],
-        feature_frequencies: Iterable[int],
-        median_uplift: Iterable[T_NumericValue],
-        low_percentile_uplift: Iterable[T_NumericValue],
-        high_percentile_uplift: Iterable[T_NumericValue],
-        low_percentile: int,
-        high_percentile: int,
-        partition_width: Optional[T_NumericValue] = None,
+        partitioning: Partitioning,
+        median_uplift: Iterable[T_Number],
+        min_uplift: Iterable[T_Number],
+        max_uplift: Iterable[T_Number],
+        min_percentile: float,
+        max_percentile: float,
     ):
-        self._feature = feature
+        self._feature_name = feature_name
         self._target_name = target_name
-        self._feature_values = feature_values
-        self._feature_frequencies = feature_frequencies
+        self._partitioning = partitioning
         self._median_uplift = median_uplift
-        self._low_percentile_uplift = low_percentile_uplift
-        self._high_percentile_uplift = high_percentile_uplift
-        self._low_percentile = low_percentile
-        self._high_percentile = high_percentile
-        self._partition_width = partition_width
+        self._min_uplift = min_uplift
+        self._max_uplift = max_uplift
+        self._min_percentile = min_percentile
+        self._max_percentile = max_percentile
 
     @property
-    def feature(self) -> str:
-        return self._feature
+    def feature_name(self) -> str:
+        return self._feature_name
 
     @property
     def target_name(self) -> str:
         return self._target_name
 
     @property
-    def feature_values(self) -> Iterable[T_NumericValue]:
-        return self._feature_values
+    def partitioning(self) -> Partitioning:
+        return self._partitioning
 
     @property
-    def feature_frequencies(self) -> Iterable[int]:
-        return self._feature_frequencies
-
-    @property
-    def median_uplift(self) -> Iterable[T_NumericValue]:
+    def median_uplift(self) -> Iterable[T_Number]:
         return self._median_uplift
 
     @property
-    def low_percentile_uplift(self) -> Iterable[T_NumericValue]:
-        return self._low_percentile_uplift
+    def min_uplift(self) -> Iterable[T_Number]:
+        return self._min_uplift
 
     @property
-    def high_percentile_uplift(self) -> Iterable[T_NumericValue]:
-        return self._high_percentile_uplift
+    def max_uplift(self) -> Iterable[T_Number]:
+        return self._max_uplift
 
     @property
-    def low_percentile(self) -> int:
-        return self._low_percentile
+    def min_percentile(self) -> float:
+        return self._min_percentile
 
     @property
-    def high_percentile(self) -> int:
-        return self._high_percentile
-
-    @property
-    def partition_width(self) -> float:
-        return self._partition_width
+    def max_percentile(self) -> float:
+        return self._max_percentile
 
 
 class UnivariateSimulator:
@@ -94,24 +82,78 @@ class UnivariateSimulator:
     :meth:`UnivariateSimulator.aggregate_simulation_results`.
 
     :param model_fit: fitted model used for the simulation
-    """
-
-    __slots__ = ["_model_fit"]
+    :param min_percentile: lower bound of the confidence interval (default: 2.5)
+    :param max_percentile: upper bound of the confidence interval (default: 97.5)
+"""
 
     F_SPLIT_ID = "split_id"
     F_PARAMETER_VALUE = "parameter_value"
     F_RELATIVE_TARGET_CHANGE = "relative_target_change"
 
-    def __init__(self, model_fit: PredictorFitCV):
+    def __init__(
+        self,
+        model_fit: PredictorFitCV,
+        min_percentile: float = 2.5,
+        max_percentile: float = 97.5,
+    ):
         self._model_fit = model_fit
+        if not 0 <= min_percentile <= 100:
+            raise ValueError(
+                f"arg min_percentile={min_percentile} must be in the range"
+                "from 0 to 100"
+            )
+        if not 0 <= max_percentile <= 100:
+            raise ValueError(
+                f"arg max_percentile={max_percentile} must be in the range"
+                "from 0 to 1"
+            )
+        self._min_percentile = min_percentile
+        self._max_percentile = max_percentile
 
     @property
     def model_fit(self) -> PredictorFitCV:
         """The fitted model used for the simulation."""
         return self._model_fit
 
+    @property
+    def min_percentile(self) -> float:
+        return self._min_percentile
+
+    @property
+    def max_percentile(self) -> float:
+        return self._max_percentile
+
     def simulate_feature(
-        self, feature_name: str, feature_values: Iterable[Any]
+        self, feature_name: str, partitioning: Partitioning
+    ) -> UnivariateSimulation:
+        """
+        Simulate average yield uplift for the given feature and for each partition in
+        the given partitioning.
+
+        :param feature_name: the feature of the simulation
+        :param partitioning: the partitioning used for the simulation
+        """
+        simulated_values = partitioning.partitions()
+        prediction_uplift = self._aggregate_simulation_results(
+            results_per_split=(
+                self._simulate_feature_with_values(
+                    feature_name=feature_name, simulated_values=simulated_values
+                )
+            )
+        )
+        return UnivariateSimulation(
+            feature_name=feature_name,
+            target_name=self._model_fit.sample.target_name,
+            partitioning=partitioning,
+            median_uplift=prediction_uplift.iloc[:, 1].to_list(),
+            min_uplift=prediction_uplift.iloc[:, 0].to_list(),
+            max_uplift=prediction_uplift.iloc[:, 2].to_list(),
+            min_percentile=self._min_percentile,
+            max_percentile=self._max_percentile,
+        )
+
+    def _simulate_feature_with_values(
+        self, feature_name: str, simulated_values: Iterable[Any]
     ) -> pd.DataFrame:
         """
         Run a simulation on a feature.
@@ -122,43 +164,42 @@ class UnivariateSimulator:
         given value, compared to the predictions on the original sample.
 
         :param feature_name: name of the feature to use in the simulation
-        :param feature_values: values to use in the simulation
+        :param simulated_values: values to use in the simulation
         :return: dataframe with three columns: `split_id`, `parameter_value` and
           `relative_target_change`.
         """
         if feature_name not in self.model_fit.sample.feature_names:
             raise ValueError(f"Feature '{feature_name}' not in sample")
 
-        results = []
+        def _simulate_values() -> Generator[Tuple[int, Any, float], None, None]:
+            for value in simulated_values:
+                # replace the simulated column with a constant value
+                synthetic_sample = FunctionTransformerDF(
+                    func=lambda x: (x.assign(**{feature_name: value})), validate=False
+                ).fit_transform_sample(self.model_fit.sample)
 
-        for value in feature_values:
-            # Replace the simulated column with a constant value.
-            synthetic_sample = FunctionTransformerDF(
-                func=lambda x: (x.assign(**{feature_name: value})), validate=False
-            ).fit_transform_sample(self.model_fit.sample)
-
-            fit_for_syn_sample = self.model_fit.copy_with_sample(
-                sample=synthetic_sample
-            )
-
-            for split_id in range(self.model_fit.n_splits):
-                predictions_for_split_hist: pd.Series = (
-                    self.model_fit.predictions_for_split(split_id=split_id)
+                fit_for_syn_sample = self.model_fit.copy_with_sample(
+                    sample=synthetic_sample
                 )
 
-                predictions_for_split_syn: pd.Series = (
-                    fit_for_syn_sample.predictions_for_split(split_id=split_id)
-                )
+                for split_id in range(self.model_fit.n_splits):
+                    predictions_for_split_hist: pd.Series = (
+                        self.model_fit.predictions_for_split(split_id=split_id)
+                    )
 
-                relative_target_change = (
-                    predictions_for_split_syn.mean(axis=0)
-                    / predictions_for_split_hist.mean(axis=0)
-                ) - 1
+                    predictions_for_split_syn: pd.Series = (
+                        fit_for_syn_sample.predictions_for_split(split_id=split_id)
+                    )
 
-                results.append((split_id, value, relative_target_change))
+                    relative_target_change = (
+                        predictions_for_split_syn.mean(axis=0)
+                        / predictions_for_split_hist.mean(axis=0)
+                    ) - 1
+
+                    yield split_id, value, relative_target_change
 
         return pd.DataFrame(
-            results,
+            data=_simulate_values(),
             columns=[
                 UnivariateSimulator.F_SPLIT_ID,
                 UnivariateSimulator.F_PARAMETER_VALUE,
@@ -166,9 +207,8 @@ class UnivariateSimulator:
             ],
         )
 
-    @staticmethod
-    def aggregate_simulation_results(
-        results_per_split: pd.DataFrame, percentiles: Iterable[int]
+    def _aggregate_simulation_results(
+        self, results_per_split: pd.DataFrame
     ) -> pd.DataFrame:
         """
         Aggregate uplift values computed by `simulate_feature`.
@@ -178,9 +218,8 @@ class UnivariateSimulator:
 
         :param results_per_split: dataframe with columns `split_id`, `parameter_value`\
           and `relative_yield_change`
-        :param percentiles: the list of percentiles
-        :return: dataframe with columns percentile_<p> where p goes through the list
-          `percentiles` and whose index is given by the parameter values
+        :return: dataframe with 3 columns `percentile_<min>`, `percentile_50`,
+          `percentile<max>` where min/max are the min and max percentiles
         """
 
         def percentile(n: int) -> Callable[[float], float]:
@@ -203,79 +242,10 @@ class UnivariateSimulator:
             .groupby(by=UnivariateSimulator.F_PARAMETER_VALUE)[
                 UnivariateSimulator.F_RELATIVE_TARGET_CHANGE
             ]
-            .agg([percentile(p) for p in percentiles])
+            .agg(
+                [
+                    percentile(p)
+                    for p in (self._min_percentile, 50, self._max_percentile)
+                ]
+            )
         )
-
-    def get_simulation(
-        self,
-        feature: str,
-        feature_values: Iterable[Any],
-        feature_frequencies: Iterable[int],
-        low_percentile: int,
-        high_percentile: int,
-        partition_width: Optional[T_NumericValue] = None,
-    ) -> UnivariateSimulation:
-        """
-        Compute a summary of the univariate simulation.
-
-        :param feature:
-        :param feature_values:
-        :param feature_frequencies:
-        :param low_percentile:
-        :param high_percentile:
-        :param partition_width:
-        :return: object holding
-        """
-        results_per_split = self.simulate_feature(
-            feature_name=feature, feature_values=feature_values
-        )
-        prediction_uplift = self.aggregate_simulation_results(
-            results_per_split=results_per_split,
-            percentiles=(low_percentile, 50, high_percentile),
-        )
-        low_percentile_uplift = prediction_uplift.iloc[:, 0].to_list()
-        median_uplift = prediction_uplift.iloc[:, 1].to_list()
-        high_percentile_uplift = prediction_uplift.iloc[:, 2].to_list()
-        univariate_simulation = UnivariateSimulation(
-            feature=feature,
-            target_name=self._model_fit.sample.target_name,
-            feature_values=feature_values,
-            feature_frequencies=feature_frequencies,
-            median_uplift=median_uplift,
-            low_percentile_uplift=low_percentile_uplift,
-            high_percentile_uplift=high_percentile_uplift,
-            low_percentile=low_percentile,
-            high_percentile=high_percentile,
-            partition_width=partition_width,
-        )
-        return univariate_simulation
-
-    def get_simulation_from_partition(
-        self,
-        partition: RangePartitioning,
-        feature: str,
-        low_percentile: int = 10,
-        high_percentile: int = 90,
-    ) -> UnivariateSimulation:
-        """
-        Compute a simulation summary from a partition and a fitted model.
-
-        :param partition: the partition used for the simulation
-        :param feature: the feature of the simulation
-        :param low_percentile: percentile used to compute the low confidence of the
-          uplift simulation
-        :param high_percentile: percentile used to compute the high confidence of the
-          uplift simulation
-        """
-        feature_values = partition.partitions()
-        feature_frequencies = partition.frequencies()
-        partition_width = partition.partition_width
-        simulation = self.get_simulation(
-            feature=feature,
-            feature_values=feature_values,
-            feature_frequencies=feature_frequencies,
-            low_percentile=low_percentile,
-            high_percentile=high_percentile,
-            partition_width=partition_width,
-        )
-        return simulation
