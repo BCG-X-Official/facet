fe8b2b28542aec34659c5528281fe072e98a053e
Ittner Jan <ittner.jan@bcg.com>
2019-07-28 16:50:02 +0200

    update handling of text size in MatplotlibStyle classes

--- src/gamma/viz/__init__.py
+++ src/gamma/viz/__init__.py
@@ -1,6 +1,7 @@
 """
 MVC-based classes for drawing charts.
 """
+import logging
 import sys
 from abc import ABC, abstractmethod
 from typing import *
@@ -9,6 +10,8 @@ from typing import TextIO
 import matplotlib.pyplot as plt
 from matplotlib import text as mt
 from matplotlib.axes import Axes
+
+log = logging.getLogger(__name__)
 
 T_Model = TypeVar("T_Model")
 T_Style = TypeVar("T_Style", bound="ChartStyle")
@@ -110,22 +113,43 @@ class MatplotStyle(ChartStyle, ABC):
         """Draw the title of the chart."""
         self.ax.set_title(label=title)
 
-    def text_extent(self, text: str, **kwargs) -> Tuple[float, float]:
+    def text_size(
+        self, text: str, x: Optional[float] = None, y: Optional[float] = None, **kwargs
+    ) -> Tuple[float, float]:
         """
-        Calculate the horizontal and vertical extend of the given text in axis units.
+        Calculate the horizontal and vertical size of the given text in axis units.
         Constructs a :class:`matplotlib.text.Text` artist then calculates it size
         relative to the axis managed by this style object (attribute `ax`)
+        For non-linear axis scales text size differs depending on placement,
+        so the intended placement (in data coordinates) should be provided
 
         :param text: text to calculate the size for
+        :param x: intended horizontal text placement (optional, defaults to left of
+            view)
+        :param y: intended vertical text placement (optional, defaults to bottom of
+            view)
         :param kwargs: additional arguments to use when constructing the
-          :class:`~matplotlib.text.Text` artist, e.g., rotation
-        :return: tuple `(width, height)` in axis units
+            :class:`~matplotlib.text.Text` artist, e.g., rotation
+        :return: tuple `(width, height)` in absolute axis units
         """
-        (x0, y0), (x1, y1) = self.ax.transData.inverted().transform(
-            mt.Text(0, 0, text, figure=self.ax.figure, **kwargs).get_window_extent(
-                self._renderer
-            )
+
+        ax = self.ax
+
+        if x is None or y is None:
+            x0, y0, _, _ = ax.dataLim.bounds
+            if x is None:
+                x = x0
+            if y is None:
+                y = y0
+
+        fig = ax.figure
+
+        extent = mt.Text(x, y, text, figure=fig, **kwargs).get_window_extent(
+            fig.canvas.get_renderer()
         )
+
+        (x0, y0), (x1, y1) = ax.transData.inverted().transform(extent)
+
         return abs(x1 - x0), abs(y1 - y0)
 
 
