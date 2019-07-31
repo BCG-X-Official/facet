6ffb86f5bf24b58d506e0e48bbe24555b7f80a7e
Martin Florent <martin.florent@bcg.com>
2019-07-31 23:18:20 +0200

    add abstract transformerDF classes for sklearn dimentionality reduction and feature selection

--- src/gamma/sklearndf/transformation/_wrapper.py
+++ src/gamma/sklearndf/transformation/_wrapper.py
@@ -76,35 +76,84 @@ class ColumnPreservingTransformerWrapperDF(
     ColumnSubsetTransformerWrapperDF[T_Transformer], Generic[T_Transformer], ABC
 ):
     """
-    Transforms a data frame keeping exactly the same columns.
+    Transform a data frame keeping exactly the same columns.
 
-    A ``ColumnPreservingTransformerWrapperDF`` does not add, remove, or rename any of the
-    input columns.
+    A ``ColumnPreservingTransformerWrapperDF`` does not add, remove, or rename any of
+    the input columns.
     """
 
     def _get_columns_out(self) -> pd.Index:
         return self.columns_in
 
 
-class BaseDimensionalityReductionWrapperDF(TransformerWrapperDF):
+class BaseDimensionalityReductionWrapperDF(
+    TransformerWrapperDF[T_Transformer], Generic[T_Transformer], ABC
+):
+    """
+    Transform data making dimensionality redcution style transform.
+    """
+
+    @abstractmethod
+    def _get_columns_out(self) -> pd.Index:
+        # return column labels for arrays returned by the fitted transformer.
+        pass
+
     def _get_columns_original(self) -> pd.Series:
         raise AttributeError(
             "columns_original is not defined for dimensionality reduction transformers"
         )
 
 
-class AnonymousDimensionalityReductionWrapperDF(BaseDimensionalityReductionWrapperDF):
+class AnonymousDimensionalityReductionWrapperDF(
+    BaseDimensionalityReductionWrapperDF[T_Transformer], Generic[T_Transformer], ABC
+):
     def _get_columns_out(self) -> pd.Index:
         # todo: implement this
         pass
 
 
-class NamedDimensionalityReductionWrapperDF(BaseDimensionalityReductionWrapperDF):
+COMPONENTS = "components_"
+
+
+class NamedDimensionalityReductionWrapperDF(
+    BaseDimensionalityReductionWrapperDF[T_Transformer], Generic[T_Transformer], ABC
+):
+    """
+    Apply dimensionality reduction on a dataframe.
+
+    The delegate transformer has a ``componnents_`` attribute of the form (
+    n_components, n_features).
+    """
+
     def _get_columns_out(self) -> pd.Index:
-        # todo: implement this
-        pass
+        if not hasattr(self.delegate_estimator, COMPONENTS):
+            raise AttributeError(
+                f"NamedDimensionalityReductionWrapperDF should have "
+                f"a {COMPONENTS} attribute but does not have. The "
+                f"delegate "
+                f"estimator is {repr(T_Transformer)}"
+            )
+        feature_format = "x_{}"
+        n_features = getattr(self.delegate_estimator, COMPONENTS).shape[0]
+        return pd.Index([feature_format.format(i) for i in range(n_features)])
 
 
-class FeatureSelectionWrapperDF:
-    # todo: implement this
-    pass
+GET_SUPPORT = "get_support"
+
+
+class FeatureSelectionWrapperDF(
+    ColumnSubsetTransformerWrapperDF[T_Transformer], Generic[T_Transformer], ABC
+):
+    def _get_columns_out(self) -> pd.Index:
+        if not (
+            hasattr(self.delegate_estimator, GET_SUPPORT)
+            and callable(getattr(self.delegate_estimator, GET_SUPPORT))
+        ):
+            raise AttributeError(
+                f"FeatureSelectionWrapperDF should have a "
+                f"{GET_SUPPORT} method but does not have. The "
+                f"delegate "
+                f"estimator is {repr(T_Transformer)}"
+            )
+        support_indices = getattr(self.delegate_estimator, GET_SUPPORT)(indices=True)
+        return self.columns_in[support_indices]
