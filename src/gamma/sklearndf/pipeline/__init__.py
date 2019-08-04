aa0afa422e85c5bc8adcd9a597f6b78785b424d1
Jan Ittner <jan@MacBook-Pro.local>
2019-08-04 14:36:10 +0200

    move Gamma custom pipelines to private submodule

--- src/gamma/sklearndf/pipeline/__init__.py
+++ src/gamma/sklearndf/pipeline/__init__.py
@@ -18,31 +18,22 @@ and ClassifierDF interfaces.
 
 import logging
 from typing import *
-from typing import Any, cast, Generic, List, Optional, Union
 
 import pandas as pd
 from pandas.core.arrays import ExtensionArray
-from sklearn.base import BaseEstimator
 from sklearn.pipeline import FeatureUnion, Pipeline
 
-from gamma import ListLike
-from gamma.sklearndf import (
-    BaseEstimatorDF,
-    BasePredictorDF,
-    ClassifierDF,
-    RegressorDF,
-    T_PredictorDF,
-    TransformerDF,
-)
+from gamma.sklearndf import BaseEstimatorDF, TransformerDF
 from gamma.sklearndf._wrapper import (
     ClassifierWrapperDF,
     RegressorWrapperDF,
     TransformerWrapperDF,
 )
+from ._model import *
 
 log = logging.getLogger(__name__)
 
-__all__ = ["FeatureUnionDF", "PipelineDF", "ModelPipelineDF"]
+__all__ = [sym for sym in dir() if sym.endswith("DF")]
 
 
 class PipelineDF(
@@ -262,123 +253,3 @@ class FeatureUnionDF(TransformerWrapperDF[FeatureUnion]):
             return pd.Index()
         else:
             return indices[0].append(other=indices[1:])
-
-
-#
-# GAMMA custom pipelines
-#
-
-
-class ModelPipelineDF(BaseEstimator, ClassifierDF, RegressorDF, Generic[T_PredictorDF]):
-    """
-    A data frame enabled pipeline with an optional preprocessing step and a
-    mandatory estimator step.
-
-    :param preprocessing: the preprocessing step in the pipeline (defaults to ``None``)
-    :param predictor: the delegate estimator used in the pipeline
-    :type predictor: :class:`.BasePredictorDF`
-    """
-
-    def __init__(
-        self, predictor: T_PredictorDF, preprocessing: Optional[TransformerDF] = None
-    ) -> None:
-        super().__init__()
-
-        if preprocessing is not None and not isinstance(preprocessing, TransformerDF):
-            raise TypeError(
-                "arg preprocessing expected to be a TransformerDF but is a "
-                f"{type(preprocessing).__name__}"
-            )
-        if not isinstance(predictor, BasePredictorDF):
-            raise TypeError(
-                "arg predictor expected to be a BasePredictorDF but is a "
-                f"{type(predictor).__name__}"
-            )
-
-        self.preprocessing = preprocessing
-        self.predictor = predictor
-
-    # noinspection PyPep8Naming
-    def fit(
-        self, X: pd.DataFrame, y: Optional[pd.Series] = None, **fit_params
-    ) -> "ModelPipelineDF[T_PredictorDF]":
-        self.predictor.fit(self._pre_fit_transform(X, y, **fit_params), y, **fit_params)
-        return self
-
-    @property
-    def is_fitted(self) -> bool:
-        return self.preprocessing.is_fitted and self.predictor.is_fitted
-
-    # noinspection PyPep8Naming
-    def predict(
-        self, X: pd.DataFrame, **predict_params
-    ) -> Union[pd.Series, pd.DataFrame]:
-        return self.predictor.predict(self._pre_transform(X), **predict_params)
-
-    # noinspection PyPep8Naming
-    def fit_predict(self, X: pd.DataFrame, y: pd.Series, **fit_params) -> pd.Series:
-        return self.predictor.fit_predict(
-            self._pre_fit_transform(X, y, **fit_params), y, **fit_params
-        )
-
-    # noinspection PyPep8Naming
-    def predict_proba(self, X: pd.DataFrame) -> Union[pd.DataFrame, List[pd.DataFrame]]:
-        return cast(ClassifierDF, self.predictor).predict_proba(self._pre_transform(X))
-
-    # noinspection PyPep8Naming
-    def predict_log_proba(
-        self, X: pd.DataFrame
-    ) -> Union[pd.DataFrame, List[pd.DataFrame]]:
-        return cast(ClassifierDF, self.predictor).predict_log_proba(
-            self._pre_transform(X)
-        )
-
-    # noinspection PyPep8Naming
-    def decision_function(self, X: pd.DataFrame) -> Union[pd.Series, pd.DataFrame]:
-        return cast(ClassifierDF, self.predictor).decision_function(
-            self._pre_transform(X)
-        )
-
-    # noinspection PyPep8Naming
-    def score(
-        self,
-        X: pd.DataFrame,
-        y: Optional[pd.Series] = None,
-        sample_weight: Optional[Any] = None,
-    ) -> float:
-        if sample_weight is None:
-            return self.predictor.score(self._pre_transform(X), y)
-        else:
-            return self.predictor.score(
-                self._pre_transform(X), y, sample_weight=sample_weight
-            )
-
-    @property
-    def classes(self) -> Optional[ListLike[Any]]:
-        return cast(ClassifierDF, self.predictor).classes
-
-    @property
-    def n_outputs(self) -> int:
-        return self.predictor.n_outputs
-
-    def _get_columns_in(self) -> pd.Index:
-        if self.preprocessing is not None:
-            return self.preprocessing.columns_in
-        else:
-            return self.predictor.columns_in
-
-    # noinspection PyPep8Naming
-    def _pre_transform(self, X: pd.DataFrame) -> pd.DataFrame:
-        if self.preprocessing is not None:
-            return self.preprocessing.transform(X)
-        else:
-            return X
-
-    # noinspection PyPep8Naming
-    def _pre_fit_transform(
-        self, X: pd.DataFrame, y: pd.Series, **fit_params
-    ) -> pd.DataFrame:
-        if self.preprocessing is not None:
-            return self.preprocessing.fit_transform(X, y, **fit_params)
-        else:
-            return X
