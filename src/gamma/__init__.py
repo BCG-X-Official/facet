0d86719c8a131a00c2143ef956fc21b1459f58ca
Joerg Schneider <schneider.joerg@bcg.com>
2019-08-09 13:14:14 +0200

    add pkgutil-style __init__.py (recommended for highest compatability)
    https://packaging.python.org/guides/packaging-namespace-packages/#pkgutil-style-namespace-packages

--- src/gamma/__init__.py
+++ src/gamma/__init__.py
@@ -1,16 +1,1 @@
-#
-# NOT FOR CLIENT USE!
-#
-# This is a pre-release library under development. Handling of IP rights is still
-# being investigated. To avoid causing any potential IP disputes or issues, DO NOT USE
-# ANY OF THIS CODE ON A CLIENT PROJECT, not even in modified form.
-#
-# Please direct any queries to any of:
-# - Jan Ittner
-# - Jörg Schneider
-# - Florent Martin
-#
-
-"""
-The Machine Learning module of Gamma's Alpha library
-"""
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
