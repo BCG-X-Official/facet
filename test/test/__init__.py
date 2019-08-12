ce71fdfde48c035727203d0971eec32a639e1d0e
Joerg Schneider <schneider.joerg@bcg.com>
2019-08-12 09:44:04 +0200

    remove obsolete comment

--- test/test/__init__.py
+++ test/test/__init__.py
@@ -5,8 +5,6 @@ from test.paths import TEST_CONFIG_YML
 
 def read_test_config(section: str = None):
     config_file_path = TEST_CONFIG_YML
-    # todo: handle exception
-    # todo: validate yaml structure (known/required keys)
     with open(config_file_path, "r") as f:
         config = yaml.safe_load(f)
         if section is None:
