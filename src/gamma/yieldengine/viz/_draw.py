061ff7e55823abf20909a9244282ed172ef5d468
Jan Ittner <ittner.jan@bcg.com>
2019-10-07 08:12:18 +0200

    add support for named styles and a default style, to simplify the instantiation of Drawer objects

--- src/gamma/yieldengine/viz/_draw.py
+++ src/gamma/yieldengine/viz/_draw.py
@@ -25,29 +25,46 @@ from gamma.common import ListLike
 from gamma.viz import Drawer
 from gamma.yieldengine.partition import T_Value
 from gamma.yieldengine.simulation import UnivariateSimulation
-from gamma.yieldengine.viz._style import SimulationStyle
+from gamma.yieldengine.viz._style import (
+    SimulationMatplotStyle,
+    SimulationReportStyle,
+    SimulationStyle,
+)
+
+
+class _SimulationSeries(NamedTuple):
+    # A set of aligned series representing the simulation result
+    median_uplift: ListLike[T_Value]
+    min_uplift: ListLike[T_Value]
+    max_uplift: ListLike[T_Value]
+    partitions: ListLike[T_Value]
+    frequencies: ListLike[T_Value]
 
 
 class SimulationDrawer(Drawer[UnivariateSimulation, SimulationStyle]):
     """
     Simulation drawer with high/low confidence intervals.
 
-    :param style: drawing style for the uplift graph and the feature histogram
-    :param histogram: if ``True`` (default) the feature histogram is plotted,
-      if ``False`` the histogram is not plotted
+    :param style: the style of the dendrogram; either as a
+        :class:`~gamma.yieldengine.viz.SimulationStyle` instance, or as the name of a \
+        default style. Permissible names are "matplot" for a style supporting \
+        Matplotlib, and "text" for a text-only report to stdout (default: `"matplot"`)
+    :param histogram: if ``True``, plot the histogram of observed values for the \
+        feature being simulated; if ``False`` do not plot the histogram (default: \
+        ``True``).
     """
 
-    class _SimulationSeries(NamedTuple):
-        # A set of aligned series representing the simulation result
-        median_uplift: ListLike[T_Value]
-        min_uplift: ListLike[T_Value]
-        max_uplift: ListLike[T_Value]
-        partitions: ListLike[T_Value]
-        frequencies: ListLike[T_Value]
+    _STYLES = {"matplot": SimulationMatplotStyle, "text": SimulationReportStyle}
 
-    def __init__(self, style: SimulationStyle, histogram: bool = True):
+    def __init__(
+        self, style: Union[SimulationStyle, str] = "matplot", histogram: bool = True
+    ) -> None:
         super().__init__(style=style)
         self._histogram = histogram
+
+    @classmethod
+    def _get_style_dict(cls) -> Mapping[str, Type[SimulationStyle]]:
+        return SimulationDrawer._STYLES
 
     def _draw(self, data: UnivariateSimulation) -> None:
         # draw the simulation chart
@@ -75,16 +92,15 @@ class SimulationDrawer(Drawer[UnivariateSimulation, SimulationStyle]):
                 is_categorical_feature=data.partitioning.is_categorical,
             )
 
-    def _get_simulation_series(
-        self, simulation: UnivariateSimulation
-    ) -> _SimulationSeries:
+    @staticmethod
+    def _get_simulation_series(simulation: UnivariateSimulation) -> _SimulationSeries:
         # return the simulation series for median uplift, min uplift, max uplift,
         # partitions and frequencies
         # If the partitioning of the simulation is categorical, the series are
         # sorted in ascending order of the median uplift.
         # Otherwise, the simulation series are returned unchanged.
 
-        simulation_series = self._SimulationSeries(
+        simulation_series = _SimulationSeries(
             simulation.median_change,
             simulation.min_change,
             simulation.max_change,
@@ -93,7 +109,8 @@ class SimulationDrawer(Drawer[UnivariateSimulation, SimulationStyle]):
         )
 
         if simulation.partitioning.is_categorical:
-            return self._SimulationSeries(
+            # for categorical features, sort the categories by the median uplift
+            return _SimulationSeries(
                 *zip(*sorted(zip(*simulation_series), key=lambda x: x[0]))
             )
         else:
