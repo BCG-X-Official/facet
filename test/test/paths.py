00744ab2ff0a11cf4b209310ada0b9c74a03d346
Joerg Schneider <schneider.joerg@bcg.com>
2019-10-25 16:43:50 +0200

    make the paths non-relative; makes pytest work from any invoc. path

--- test/test/paths.py
+++ test/test/paths.py
@@ -4,8 +4,9 @@ import os
 log = logging.getLogger(__name__)
 
 # directory paths
-DIR_DATA = "data"
-DIR_CONFIG = "config"
+PROJECT_ROOT = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir, os.pardir))
+DIR_DATA = os.path.join(PROJECT_ROOT, "data")
+DIR_CONFIG = os.path.join(PROJECT_ROOT, "config")
 
 # file paths
 TEST_DATA_CSV = os.path.join(DIR_DATA, "master_table_clean_anon_144.csv")
