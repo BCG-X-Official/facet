42efa0c6d3f49673cc783a800bebd0470e8a75f6
Jan Ittner <ittner.jan@bcg.com>
2020-07-23 22:20:15 +0200

    add test for actuals simulation

--- test/test/gamma/yieldengine/test_simulation.py
+++ test/test/gamma/yieldengine/test_simulation.py
@@ -1,15 +1,14 @@
 import logging
-from typing import *
 
 import pandas as pd
-from pandas.util.testing import assert_frame_equal
+import pytest
+from pandas.util.testing import assert_frame_equal, assert_series_equal
 
 # noinspection PyPackageRequirements
 from pytest import approx
 
 from gamma.ml import Sample
 from gamma.ml.crossfit import LearnerCrossfit
-from gamma.ml.selection import ParameterGrid
 from gamma.ml.validation import StationaryBootstrapCV
 from gamma.sklearndf import TransformerDF
 from gamma.sklearndf.pipeline import RegressorPipelineDF
@@ -22,16 +21,12 @@ log = logging.getLogger(__name__)
 N_SPLITS = 10
 
 
-def test_univariate_simulation(
-    batch_table: pd.DataFrame,
-    regressor_grids: Iterable[ParameterGrid],
-    sample: Sample,
-    simple_preprocessor: TransformerDF,
-    n_jobs,
-) -> None:
-
+@pytest.fixture
+def crossfit(
+    sample: Sample, simple_preprocessor: TransformerDF, n_jobs: int
+) -> LearnerCrossfit:
     # use a pre-optimised model
-    crossfit = LearnerCrossfit(
+    return LearnerCrossfit(
         pipeline=RegressorPipelineDF(
             preprocessing=simple_preprocessor,
             regressor=LGBMRegressorDF(
@@ -40,9 +35,15 @@ def test_univariate_simulation(
         ),
         cv=StationaryBootstrapCV(n_splits=N_SPLITS, random_state=42),
         shuffle_features=False,
+        n_jobs=n_jobs,
     ).fit(sample=sample)
 
-    simulator = UnivariateUpliftSimulator(
+
+@pytest.fixture
+def uplift_simulator(
+    crossfit: LearnerCrossfit, n_jobs: int
+) -> UnivariateUpliftSimulator:
+    return UnivariateUpliftSimulator(
         crossfit=crossfit,
         min_percentile=10,
         max_percentile=90,
@@ -50,9 +51,39 @@ def test_univariate_simulation(
         verbose=50,
     )
 
+
+def test_actuals_simulation(uplift_simulator: UnivariateUpliftSimulator) -> None:
+
+    assert_series_equal(
+        uplift_simulator.simulate_actuals(),
+        pd.Series(
+            index=pd.RangeIndex(10, name="crossfit_id"),
+            data=[
+                0.0022346580154160023,
+                0.002617709072003871,
+                0.0007107764530731586,
+                -0.005023988425882142,
+                0.004010384123196653,
+                0.006379100635514501,
+                0.0005625736529850656,
+                -0.0018641287749763258,
+                0.0038418237873085737,
+                -0.0025806445857182725,
+            ],
+            name="relative deviations of mean predictions from mean targets",
+        ),
+    )
+
+
+def test_univariate_uplift_simulation(
+    uplift_simulator: UnivariateUpliftSimulator
+) -> None:
+
     parameterized_feature = "Step4-6 RawMat Vendor Compound08 Purity (#)"
 
-    res = simulator._simulate_feature_with_values(
+    sample = uplift_simulator.crossfit.training_sample
+
+    res = uplift_simulator._simulate_feature_with_values(
         feature_name=parameterized_feature,
         simulated_values=ContinuousRangePartitioner()
         .fit(values=sample.features.loc[:, parameterized_feature])
@@ -69,7 +100,9 @@ def test_univariate_simulation(
     assert absolute_target_change_sr.mean() == approx(-0.05250827061702283)
     assert absolute_target_change_sr.max() == approx(0.8872351297966503)
 
-    aggregated_results = simulator._aggregate_simulation_results(results_per_split=res)
+    aggregated_results = uplift_simulator._aggregate_simulation_results(
+        results_per_crossfit=res
+    )
     log.debug(aggregated_results)
 
     # test the first five rows of aggregated_results
