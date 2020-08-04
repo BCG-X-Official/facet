5d302271e390f6b77a594e68267a696e8d1455cf
Jan Ittner <ittner.jan@bcg.com>
2020-08-04 22:59:48 +0200

    remove old "not for client use" comments

--- src/gamma/yieldengine/viz/_style.py
+++ src/gamma/yieldengine/viz/_style.py
@@ -1,16 +1,3 @@
-#
-# NOT FOR CLIENT USE!
-#
-# This is a pre-release library under development. Handling of IP rights is still
-# being investigated. To avoid causing any potential IP disputes or issues, DO NOT USE
-# ANY OF THIS CODE ON A CLIENT PROJECT, not even in modified form.
-#
-# Please direct any queries to any of:
-# - Jan Ittner
-# - Jörg Schneider
-# - Florent Martin
-#
-
 """
 Simulation drawing styles
 
