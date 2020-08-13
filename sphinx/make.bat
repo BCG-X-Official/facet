a266fa63f42108661ecd006e4935a00df69f2ac6
Jason Bentley <Bentley.Jason@bcg.com>
2020-08-13 17:05:03 +0100

    minor updates

--- sphinx/make.bat
+++ sphinx/make.bat
@@ -1,5 +1,4 @@
 @ECHO OFF
-
 pushd %~dp0
 
 REM Command file for Sphinx documentation
@@ -9,8 +8,12 @@ if "%SPHINXBUILD%" == "" (
 )
 set SOURCEDIR=source
 set BUILDDIR=build
+REM say to apidoc which members to consider
+set SPHINX_APIDOC_OPTIONS=members,undoc-members,inherited-members
+
 
 if "%1" == "" goto help
+if "%1" == "html" goto html
 
 %SPHINXBUILD% >NUL 2>NUL
 if errorlevel 9009 (
@@ -25,11 +28,29 @@ if errorlevel 9009 (
 	exit /b 1
 )
 
-%SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+%SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS%
+goto end
+
+:html
+REM generate apidoc using docstrings
+sphinx-apidoc -e --no-toc -o %SOURCEDIR% -f ../src
+
+REM run the sphinx build for html
+%SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS%
+REM run sphinx for notebook
+REM %SPHINXBUILD% -b %1 -c %SOURCEDIR% %NOTEBOOK_QUICK_START% %BUILDDIR% %SPHINXOPTS%
+REM clean up potentially pre-existing files in /docs
+del /q /s ..\docs\* >nul
+for /d %%i in (..\docs\*) do rd /s /q "%%i"
+REM move the last build into /docs
+set DIR_HTML=build\html
+set DIR_DOCS=..\docs
+for %%i in (%DIR_HTML%\*) do move "%%i" %DIR_DOCS%\ >nul
+for /d %%i in (%DIR_HTML%\*) do move "%%i" %DIR_DOCS%\ >nul
 goto end
 
 :help
-%SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+%SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS%
 
 :end
 popd
