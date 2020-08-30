bfe09f99721fd87fec3c5df1a5a25f4065e7227a
Jan Ittner <ittner.jan@bcg.com>
2020-08-30 17:36:07 +0200

    initial implementation of UnivariateProbabilitySimulator (#28)
    * change BaseUnivariateSimulator properties into simple attributes
    
    * remove abstract methods from the base simulator
    
    … their signatures and behaviours will differ across subclasses
    
    * make UnivariateSimulation a NamedTuple and use more general field names
    
    * initial implementation of UnivariateProbabilitySimulator
    
    - unit tests still to be written
    - docstrings to be reviewed/updated
    
    * add UnivariateProbabilitySimulator to __all__
    
    * add logging to the simulation module
    
    * re-arrange header section of the simulation module
    
    * in BaseUnivariateSimulator.__init__, ensure a single-output pipeline
    
    * correct type hinting in the simulation module
    
    * implement method simulate_actuals for univariate probability simulations
    
    * update field names for simulated values and percentiles
    
    * add field values_label to UnivariateSimulation class
    
    * replace crossfit.training_sample with crossfit.sample
    
    * render a simulation report in the univariate uplift simulator test
    
    * comprehensively overhaul docstrings of facet.simulation and subpackages

--- src/facet/simulation/partition/__init__.py
+++ src/facet/simulation/partition/__init__.py
@@ -1,18 +1,9 @@
 """
-Partitioner for collections of categorical or numerical values.
+Partitioners to generate series of numerical and categorical values to be used
+as inputs for simulations.
 
-:class:`Partitioner` partitions a set of values
-into finitely many partitions (synonym of buckets).
-:meth:`~Partitioner.frequencies` returns  an iterable of the number of
-values in the different partitions, :meth:`~Partitioner.partitions`
-returns a list of central value in each partition,
-and :attr:`~Partitioner.n_partitions` is the number of partitions.
+Numerical partitions are intervals, represented by their central value.
 
-:class:`ContinuousRangePartitioner` is adapted set of floats
-
-:class:`IntegerRangePartitioner` is adapted to sets of  integers; the bounds of the
-partitions are integers
-
-:class:`CategoryPartitioner` is adapted to categorical sets
+Categorical partitions are the categories themselves.
 """
 from ._partition import *
