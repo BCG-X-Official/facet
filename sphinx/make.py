850802501dc61c0b463f39094e8a136ffcb2bc69
Jan Ittner <ittner.jan@bcg.com>
2020-09-25 09:01:51 +0200

    refactor make.py proxy script

--- sphinx/make.py
+++ sphinx/make.py
@@ -6,13 +6,20 @@ Make sphinx documentation using the makefile in pytools
 import os
 import sys
 
-if __name__ == "__main__":
 
+def run_make() -> None:
+    """
+    Run the common make file available in the pytools repo
+    """
     cwd = os.path.dirname(__file__)
     os.chdir(cwd)
 
-    sys.path.append(os.path.realpath(os.path.join(cwd, os.pardir, os.pardir)))
+    sys.path.append(os.path.normpath(os.path.join(cwd, os.pardir, os.pardir)))
     # noinspection PyUnresolvedReferences
     from pytools.sphinx.make import run_make
 
+    run_make()
+
+
+if __name__ == "__main__":
     run_make()
