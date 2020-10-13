1eb7a43af74efb09012bac2153b4de246de7cfb2
mgelsm <grisard.malo@bcg.com>
2020-10-13 17:44:14 +0200

    fix API landing page bullet points arrangement

--- src/facet/__init__.py
+++ src/facet/__init__.py
@@ -5,22 +5,18 @@ inspection.
 
 Implements the following subpackages:
 
-- :mod:`facet.selection`: model selection and hyperparameter
-tuning
-- :mod:`facet.validation`: scikit-learn-style cross-validators
-for different \
-    bootstrapping approaches
+- :mod:`facet.selection`: model selection and hyperparameter-tuning
+- :mod:`facet.validation`: scikit-learn-style cross-validators \
+for different bootstrapping approaches
 - :mod:`facet.crossfit`: a `crossfit` object manages multiple
-fits \
-    of the same learner across all splits of a cross-validator,
-    enabling a range of \
-    approaches for model selection, inspection, and simulation/
-    optimization
-- :mod:`facet.inspection`: explaining the interactions of a model's
-features \
-    with each other, and with the target variable, based on the SHAP
-    approach
-- :mod:`facet.simulation`: simulating variables using a predictive
+fits of the same learner across all splits of a cross-validator, \
+enabling a range of \
+approaches for model selection, inspection, and simulation/
+optimization
+- :mod:`facet.inspection`: explaining the interactions of a model's \
+features with each other, and with the target variable, based on the SHAP \
+approach
+- :mod:`facet.simulation`: simulating variables using a predictive \
 model
 """
 
