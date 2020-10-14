9c5ad01c91ec2e73a84bbdb597a020633372705d
Jason Bentley <Bentley.Jason@bcg.com>
2020-10-14 16:44:06 +0100

    revise landing page for API reference

--- src/facet/__init__.py
+++ src/facet/__init__.py
@@ -1,31 +1,17 @@
 """
-Machine learning workflows for advanced model selection and
-inspection.
+
+Welcome to the class and function reference of FACET to enhance your machine learning
+workflow for advanced model selection and inspection.
 
 
-Implements the following subpackages:
+.. image::
+    ../_static/Facet_flow.svg
 
-- :mod:`facet.selection`: model selection and hyperparameter
-tuning
-- :mod:`facet.validation`: scikit-learn-style cross-validators
-for different \
-    bootstrapping approaches
-- :mod:`facet.crossfit`: a `crossfit` object manages multiple
-fits \
-    of the same learner across all splits of a cross-validator,
-    enabling a range of \
-    approaches for model selection, inspection, and simulation/
-    optimization
-- :mod:`facet.inspection`: explaining the interactions of a model's
-features \
-    with each other, and with the target variable, based on the SHAP
-    approach
-- :mod:`facet.simulation`: simulating variables using a predictive
-model
+
+Please refer to the tutorials for further information on the use of FACET classes and
+functions.
+
 """
-
-# todo: explain the basic workflow in the docstring:
-#       LearnerRanker --> LearnerCrossfit --> LearnerInspector
 
 
 from ._facet import Sample
