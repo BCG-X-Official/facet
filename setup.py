2b80e80f39b10301584f4b93b19709cde4ee46d3
Jörg Schneider <46053259+joerg-schneider@users.noreply.github.com>
2020-10-28 10:16:13 +0100

    Move to Python make script and link version strings (#124)
    * build: setup.py reference package's __version__
    * build: move meta.yaml to condabuild/; reference setup.py version
    * build: add make.py for facet package calling pytools' make script
    * build: remove bsd/gnu Makefile, use make.py in azure-pipelines.yml
    * build: add x flag to make.py
    * build: update azure-pipelines.yml

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ import os
 
 from setuptools import find_packages, setup
 
+from src.facet import __version__
+
 here = os.path.dirname(os.path.realpath(__file__))
 # Get the long description from the README file
 with open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
@@ -29,7 +31,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="1.0.0",
+    version=__version__,
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
