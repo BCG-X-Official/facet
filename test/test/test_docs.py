b05e8222f52fc41dc076e4054d8b071626b83014
Jan Ittner <github.com@ittner.eu>
2021-02-19 17:04:24 +0100

    FIX: make documentation test work again

--- test/test/test_docs.py
+++ test/test/test_docs.py
@@ -4,7 +4,8 @@ import logging
 import os
 import re
 from glob import glob
-from typing import List, Tuple
+from types import ModuleType
+from typing import Any, Iterable, List, Optional, Tuple
 
 # noinspection PyPackageRequirements
 import pytest
@@ -19,22 +20,32 @@ def all_script_files() -> List[str]:
 
     :return: List of relative paths
     """
-    start_dir = os.path.join(os.pardir, "yieldengine")
-    return [y for x in os.walk(start_dir) for y in glob(os.path.join(x[0], "*.py"))]
+    start_dir = "src"
+    suffix = ".py"
+    prefix_len = len(start_dir) + len(os.sep)
+    suffix_len = len(suffix)
+
+    return [
+        path[prefix_len:-suffix_len]
+        for path in glob(os.path.join(start_dir, "**", f"*{suffix}"), recursive=True)
+    ]
 
 
 @pytest.fixture
-def all_script_objects(all_script_files) -> List[object]:
+def all_script_objects(all_script_files: List[str]) -> List[Tuple[str, ModuleType]]:
     """
     Dynamically import all Python modules in the codebase
 
     :param all_script_files: List of all relevant script paths
     :return: List of imported Python modules as objects
     """
-    modules = [
-        s[3:-3].replace("/", ".").replace(".__init__", "") for s in all_script_files
+    return [
+        (module, importlib.import_module(module))
+        for module in (
+            path.replace(os.sep, ".").replace(".__init__", "")
+            for path in all_script_files
+        )
     ]
-    return [(m, importlib.import_module(m)) for m in modules]
 
 
 # add function, method, or class names in here to ignore from being checked
@@ -53,8 +64,8 @@ IGNORE_LIST_FUNCTION = [
 IGNORE_LIST_METHOD = []
 
 
-def member_childs(
-    member: object, module_filter: None
+def member_children(
+    member: Any, module_filter: Optional[str]
 ) -> Tuple[
     List[Tuple[str, object]], List[Tuple[str, object]], List[Tuple[str, object]]
 ]:
@@ -65,26 +76,28 @@ def member_childs(
     :return: three lists for classes, functions and methods
     """
 
-    all_childs = inspect.getmembers(member)
+    all_children = inspect.getmembers(member)
 
     if module_filter is not None:
-        all_childs = [
+        all_children = [
             c
-            for c in all_childs
+            for c in all_children
             if hasattr(c[1], "__module__") and c[1].__module__ == module_filter
         ]
 
     classes = [
-        c for c in all_childs if inspect.isclass(c[1]) and c[0] not in IGNORE_LIST_CLASS
+        c
+        for c in all_children
+        if inspect.isclass(c[1]) and c[0] not in IGNORE_LIST_CLASS
     ]
     functions = [
         c
-        for c in all_childs
+        for c in all_children
         if inspect.isfunction(c[1]) and c[0] not in IGNORE_LIST_FUNCTION
     ]
     methods = [
         c
-        for c in all_childs
+        for c in all_children
         if inspect.ismethod(c[1]) and c[0] not in IGNORE_LIST_METHOD
     ]
 
@@ -98,11 +111,11 @@ def docstr_missing(obj_name: str, obj: object) -> bool:
     :param obj: object to check
     :return: boolean if docstr is missing
     """
-    if hasattr(obj, "__doc__") and not obj_name.startswith("__"):
-        if obj.__doc__ is None or len(str(obj.__doc__).strip()) < 1:
-            return True
+    if obj_name.startswith("_"):
+        return False
 
-    return False
+    doc = getattr(obj, "__doc__", None)
+    return not (doc and str(doc).strip())
 
 
 def extract_params_from_docstr(docstr: str) -> List[str]:
@@ -138,7 +151,7 @@ def parameters_inconsistent(parent: str, call_obj_name: str, call_obj: object) -
     if docstr_params is not None and len(docstr_params) > 0:
 
         for idx, f_arg in enumerate(func_args):
-            if idx > len(docstr_params) or docstr_params[idx] != f_arg:
+            if idx >= len(docstr_params) or docstr_params[idx] != f_arg:
                 log.info(
                     f"Wrong arguments in docstr for {parent}.{call_obj_name}: {f_arg}"
                 )
@@ -155,6 +168,9 @@ def parameters_inconsistent(parent: str, call_obj_name: str, call_obj: object) -
 
 def test_docstrings(all_script_objects) -> None:
 
+    if not all_script_objects:
+        raise ValueError("no script objects found")
+
     classes_with_missing_docstr = []
     functions_with_missing_docstr = []
     methods_with_missing_docstr = []
@@ -163,67 +179,103 @@ def test_docstrings(all_script_objects) -> None:
 
     for module_name, module in all_script_objects:
 
-        classes, functions, methods = member_childs(module, module_filter=module_name)
+        def full_name(name: str) -> str:
+            return f"{module_name}.{name}"
+
+        classes, functions, methods = member_children(module, module_filter=module_name)
 
         # classes where docstring is None:
         classes_with_missing_docstr.extend(
-            [name for name, obj in classes if docstr_missing(name, obj)]
+            full_name(name) for name, obj in classes if docstr_missing(name, obj)
         )
 
         # functions where docstring is None:
         functions_with_missing_docstr.extend(
-            [name for name, obj in functions if docstr_missing(name, obj)]
+            full_name(name) for name, obj in functions if docstr_missing(name, obj)
         )
 
         # methods where docstring is None:
         methods_with_missing_docstr.extend(
-            [name for name, obj in methods if docstr_missing(name, obj)]
+            full_name(name) for name, obj in methods if docstr_missing(name, obj)
         )
 
-        for f_name, f_obj in functions:
-            if not docstr_missing(f_name, f_obj) and parameters_inconsistent(
-                module_name, f_name, f_obj
-            ):
-                inconsistent_parameters.append(f_name)
+        inconsistent_parameters.extend(
+            full_name(name)
+            for name, func in functions
+            if not (name.startswith("_") or docstr_missing(name, func))
+            and parameters_inconsistent(
+                parent=module_name, call_obj_name=name, call_obj=func
+            )
+        )
 
         # inspect found classes:
-        for clsname, cls in classes:
-            _inner_classes, inner_functions, inner_methods = member_childs(cls, None)
+        for cls_name, cls in classes:
+
+            if cls_name.startswith("_"):
+                continue
+
+            _inner_classes, inner_functions, inner_methods = member_children(cls, None)
+
+            def full_name(name: str) -> str:
+                return f"{module_name}.{cls_name}.{name}"
 
             # functions where docstring is None:
             functions_with_missing_docstr.extend(
-                [name for name, obj in inner_functions if docstr_missing(name, obj)]
+                [
+                    full_name(name)
+                    for name, obj in inner_functions
+                    if docstr_missing(name, obj)
+                ]
             )
 
-            for f_name, f_obj in inner_functions:
-                if not docstr_missing(f_name, f_obj) and parameters_inconsistent(
-                    clsname, f_name, f_obj
-                ):
-                    inconsistent_parameters.append(clsname + "." + f_name)
+            inconsistent_parameters.extend(
+                full_name(name)
+                for name, func in inner_functions
+                if not (name.startswith("_") or docstr_missing(name, func))
+                and parameters_inconsistent(
+                    parent=f"{module_name}.{cls_name}",
+                    call_obj_name=name,
+                    call_obj=func,
+                )
+            )
 
             # methods where docstring is None:
             methods_with_missing_docstr.extend(
-                [name for name, obj in inner_methods if docstr_missing(name, obj)]
+                [
+                    full_name(name)
+                    for name, obj in inner_methods
+                    if docstr_missing(name, obj)
+                ]
             )
 
+    def lines(s: Iterable[str]) -> str:
+        return "\n".join(s)
+
     if classes_with_missing_docstr:
-        log.info(f"The following classes lack-docstrings:{classes_with_missing_docstr}")
+        log.info(
+            "The following classes lack docstrings:\n"
+            + lines(classes_with_missing_docstr)
+        )
 
     if functions_with_missing_docstr:
         log.info(
-            f"The following functions lack-docstrings:{functions_with_missing_docstr}"
+            "The following functions lack docstrings:\n"
+            + lines(functions_with_missing_docstr)
         )
 
     if methods_with_missing_docstr:
-        log.info(f"The following methods lack-docstrings:{methods_with_missing_docstr}")
+        log.info(
+            "The following methods lack docstrings:\n"
+            + lines(methods_with_missing_docstr)
+        )
 
     if inconsistent_parameters:
         log.info(
-            f"The following methods have inconsistently described parameters:"
-            f"{inconsistent_parameters}"
+            "The following methods have inconsistently described parameters:\n"
+            + lines(inconsistent_parameters)
         )
 
-    assert len(classes_with_missing_docstr) == 0
-    assert len(functions_with_missing_docstr) == 0
-    assert len(methods_with_missing_docstr) == 0
-    assert len(inconsistent_parameters) == 0
+    assert not classes_with_missing_docstr
+    assert not functions_with_missing_docstr
+    assert not methods_with_missing_docstr
+    assert not inconsistent_parameters
