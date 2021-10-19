96898b9d16e6db4344784c799518d836cbf3ccae
Jan Ittner <github.com@ittner.eu>
2021-10-19 11:26:52 +0200

    TEST: remove obsolete helper functions

--- test/test/facet/test_shap_decomposition.py
+++ test/test/facet/test_shap_decomposition.py
@@ -2,7 +2,7 @@
 Test shap decomposition calculations
 """
 import logging
-from typing import Set, Union
+from typing import Set
 
 import numpy as np
 
@@ -40,67 +40,3 @@ def test_feature_affinity_matrices(
         assert (
             np.nanmin(matrix.values) >= 0.0 and np.nanmax(matrix.values) <= 1.0
         ), f"Values of [0.0, 1.0] in {matrix_full_name}"
-
-
-#
-# auxiliary functions
-#
-
-
-def cov(a: np.ndarray, b: np.ndarray) -> float:
-    """
-    covariance, assuming a population mean of 0
-    :param a: array of floats
-    :param b: array of floats
-    :return: covariance of a and b
-    """
-    return (a * b).mean()
-
-
-def var(a: np.ndarray) -> float:
-    """
-    variance, assuming a population mean of 0
-    :param a: array of floats
-    :return: variance of a
-    """
-    return cov(a, a)
-
-
-def std(a: np.ndarray) -> float:
-    """
-    standard deviation, assuming a population mean of 0
-    :param a: array of floats
-    :return: standard deviation of a
-    """
-    return np.sqrt(var(a))
-
-
-def corr(a: np.ndarray, b: np.ndarray) -> float:
-    """
-    pearson correlation, assuming a population mean of 0
-    :param a: array of floats
-    :param b: array of floats
-    :return: pearson correlation of a and b
-    """
-    return cov(a, b) / np.sqrt(var(a) * var(b))
-
-
-def print_list(*args, percentage: bool = False, **kwargs):
-    """
-    print all arguments, including their names
-    :param args: the arguments to print (as their names, print integers indicating \
-        the position)
-    :param percentage: if `true`, print all arguments as % values
-    :param kwargs: the named arguments to print
-    :return:
-    """
-
-    def _prt(_value, _name: Union[str, int]):
-        if percentage:
-            _value *= 100
-        print(f"{_name}: {_value:.4g}{'%' if percentage else ''}")
-
-    for name, arg in enumerate(args):
-        _prt(arg, _name=name)
-    for name, arg in kwargs.items():
-        _prt(arg, _name=name)
