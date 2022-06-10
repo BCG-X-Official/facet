9db87fafc40c1c04ec833aedd43895cc0653b60b
Ben Howitt <howitt.ben@bcg.com>
2022-06-10 13:08:28 +0100

    add test in make.py to check presence of local pytools repo

--- sphinx/make.py
+++ sphinx/make.py
@@ -14,11 +14,15 @@ def make() -> None:
     cwd = os.path.dirname(os.path.realpath(__file__))
     os.chdir(cwd)
 
+    local_pytools_path = os.path.normpath(
+        os.path.join(cwd, os.pardir, os.pardir, "pytools", "sphinx", "base")
+    )
+    if not os.path.exists(local_pytools_path):
+        raise ModuleNotFoundError(f"Local pytools repo is needed to build docs, but"
+                                  f"was not found in {local_pytools_path}")
     sys.path.insert(
         0,
-        os.path.normpath(
-            os.path.join(cwd, os.pardir, os.pardir, "pytools", "sphinx", "base")
-        ),
+        local_pytools_path,
     )
     # noinspection PyUnresolvedReferences
     from make_base import make
