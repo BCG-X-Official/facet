3e801b709839e227da295d13a8574170f95302eb
Ben Howitt <howitt.ben@bcg.com>
2022-06-14 10:44:55 +0100

    [docs] revert branch changes to conf.py

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -7,17 +7,21 @@ Receives majority of configuration from pytools conf_base.py
 import os
 import sys
 
-
-local_pytools_path = os.path.join(
-    os.path.dirname(__file__),
-    os.pardir,
-    os.pardir,
-    os.pardir,
-    "pytools",
-    "sphinx",
-    "base",
+sys.path.insert(
+    0,
+    os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            os.pardir,
+            os.pardir,
+            os.pardir,
+            "pytools",
+            "sphinx",
+            "base",
+        )
+    ),
 )
-sys.path.insert(0, local_pytools_path)
+
 from conf_base import set_config
 
 # ----- custom configuration -----
@@ -25,6 +29,6 @@ from conf_base import set_config
 set_config(
     globals(),
     project="facet",
-    modules=["facet"],
+    modules=["facet", "pytools", "sklearndf"],
     html_logo="_static/Gamma_Facet_Logo_RGB_LB.svg",
 )
