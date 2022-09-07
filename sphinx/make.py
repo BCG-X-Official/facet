396cb273f6f66673c6072d0b2880236221bb48d7
j-ittner <ittner.jan@bcg.com>
2022-09-07 19:39:53 +0200

    DOC: make Sphinx build independent of the current working directory

--- sphinx/make.py
+++ sphinx/make.py
@@ -3,8 +3,6 @@
 Make sphinx documentation using the pytools make utility
 """
 import os
-import sys
-from typing import Callable
 from urllib import request
 
 BRANCH = "2.0.x"
@@ -12,21 +10,15 @@ BRANCH = "2.0.x"
 
 if __name__ == "__main__":
 
-    # make sure that the current working directory is where this script lives
-    wd_expected = os.path.realpath(os.path.dirname(__file__))
-    if os.path.realpath(os.getcwd()) != wd_expected:
-        print(
-            "Error: working directory is not " f"{wd_expected}",
-            file=sys.stderr,
-        )
-        sys.exit(1)
+    # noinspection PyUnusedLocal
+    def run_make(branch: str, working_directory: str) -> None:
+        """Stub, overwritten by bootstrap.py"""
 
     # run the common make file available in the pytools repo
     with request.urlopen(
         f"https://raw.githubusercontent.com/BCG-Gamma/pytools/{BRANCH}"
         f"/sphinx/base/bootstrap.py"
     ) as response:
-        run_make: Callable[[str], None] = lambda branch: None
         exec(response.read().decode("utf-8"), globals())
 
-    run_make(BRANCH)
+    run_make(branch=BRANCH, working_directory=os.path.dirname(__file__))
