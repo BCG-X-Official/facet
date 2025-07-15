d388919b8b13a477ce890314053ee06139a8cbb1
j-ittner <ittner.jan@bcg.com>
2025-07-15 22:45:23 +0200

    RF: refactor type aliasing for catboost import handling in _types.py

--- src/facet/explanation/_types.py
+++ src/facet/explanation/_types.py
@@ -1,24 +1,29 @@
 """
 Type aliases and constants for common use in the ``facet.explanation`` package
 """
-from typing import Any, List, Union
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any, List, Union
 
 import numpy as np
 import numpy.typing as npt
 import pandas as pd
 from typing_extensions import TypeAlias
 
-try:
+if TYPE_CHECKING:
     import catboost
-except ImportError:
-    from types import ModuleType
+else:
+    try:
+        import catboost
+    except ImportError:
+        from types import ModuleType
 
-    catboost = ModuleType("catboost")
-    catboost.Pool = type("Pool", (), {})
-
+        catboost = ModuleType("catboost")
+        catboost.Pool = type("Pool", (), {})
 
 ArraysAny: TypeAlias = Union[npt.NDArray[Any], List[npt.NDArray[Any]]]
 ArraysFloat: TypeAlias = Union[npt.NDArray[np.float64], List[npt.NDArray[np.float64]]]
-CatboostPool = catboost.Pool
+CatboostPool: TypeAlias = catboost.Pool
 XType: TypeAlias = Union[npt.NDArray[Any], pd.DataFrame, catboost.Pool]
 YType: TypeAlias = Union[npt.NDArray[Any], pd.Series, None]
