9ec06d4b51c023e866d389320e1f7c1d0c49ec38
j-ittner <ittner.jan@bcg.com>
2025-09-12 16:12:10 +0200

    BUILD: update version to 2.3.0

--- src/facet/__init__.py
+++ src/facet/__init__.py
@@ -5,7 +5,7 @@ This is the class and function reference of FACET for advanced model selection,
 inspection, and simulation.
 """
 
-__version__ = "2.2.3"
+__version__ = "2.3.0"
 
 __logo__ = (
     r"""
